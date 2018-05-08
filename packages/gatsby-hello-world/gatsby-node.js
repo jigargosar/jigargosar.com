129f7bc4b8c450e8f1c1ce41cd68ef4b17ed0725
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-08 12:19:40 +0530

    feat(blog): renaming

--- packages/gatsby-hello-world/gatsby-node.js
+++ packages/gatsby-hello-world/gatsby-node.js
@@ -28,7 +28,7 @@ exports.createPages = ({graphql, boundActionCreators}) => {
       result.data.allMarkdownRemark.edges.forEach(({node}) => {
         createPage({
           path: node.fields.slug,
-          component: path.resolve(`./src/templates/posts.js`),
+          component: path.resolve(`./src/templates/blog-post.js`),
           context: {
             slug: node.fields.slug,
           },
