c44b1d480405c3007cbd568dad14dff3d8b75b51
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-13 13:37:57 +0530

    feat(layout): extract function and import graphql

--- packages/gatsby-hello-world/src/layouts/index.js
+++ packages/gatsby-hello-world/src/layouts/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import {CommonHeader} from '../components/CommonHeader'
+import graphql from 'graphql'
 
-export default ({location, children, data}) =>
-  <div style={{margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem`}}>
+function Layout({location, children, data}) {
+  return <div style={{margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem`}}>
     <Helmet title={`${data.site.siteMetadata.title}`}/>
     {location.pathname === '/' || <CommonHeader title={data.site.siteMetadata.title}/>}
-    {!!console.log(location)}
+    {/*{!!console.log(location)}*/}
     {children()}
   </div>
+}
+export default Layout
 
-// noinspection JSUnresolvedVariable
 export const query = graphql`
   query LayoutQuery {
     site {
