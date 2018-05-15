393376aa5bb7023ebdfd2c4bf2039abddf39452a
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-15 17:19:44 +0530

    feat(choo): idea module and code fix

--- packages/choo-jigargosar.com/index.js
+++ packages/choo-jigargosar.com/index.js
@@ -4,10 +4,10 @@ var choo = require('choo')
 css('tachyons')
 
 var app = choo()
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
+if (process.env.NODE_ENV === 'production') {
   app.use(require('choo-service-worker')())
+} else {
+  app.use(require('choo-devtools')())
 }
 
 app.use(require('./stores/clicks'))
