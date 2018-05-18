c8cf043e05a5bd48dd3f99d291240ecfbf7cbf5e
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-18 09:07:49 +0530

    feat(spectacle): adding boilerplate to try to create presentations

--- packages/spectacle-boilerplate/presentation/index.js
+++ packages/spectacle-boilerplate/presentation/index.js
@@ -2,7 +2,16 @@
 import React from "react"
 // Import Spectacle Core tags
 import {
-  BlockQuote, Cite, Deck, Heading, List, ListItem, Quote, Slide, Text,
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  List,
+  ListItem,
+  MarkdownSlides,
+  Quote,
+  Slide,
+  Text,
 } from "spectacle"
 // Import theme
 import createTheme from "spectacle/lib/themes/default"
@@ -24,6 +33,18 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+        {MarkdownSlides`
+## Hi
+* List item
+* another
+* nice ;)
+---
+## M2
+### M2 sub
+
+Just Some text paragraph
+
+        `}
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Spectacle Boilerplate
