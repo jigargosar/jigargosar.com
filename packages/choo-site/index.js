71ff95c984a19c4ba557bc336d9964ee02a2dd51
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-18 11:04:46 +0530

    feat(choo): adding test to check rendering performance

--- packages/choo-site/index.js
+++ packages/choo-site/index.js
@@ -1,18 +1,23 @@
-var css = require('sheetify')
-var choo = require('choo')
+var css = require("sheetify")
+var choo = require("choo")
 
-css('tachyons')
+css("tachyons")
 
 var app = choo()
-if (process.env.NODE_ENV === 'production') {
-  app.use(require('choo-service-worker')())
+if (process.env.NODE_ENV === "production") {
+  app.use(require("choo-service-worker")())
 } else {
-  app.use(require('choo-devtools')())
+  app.use(require("choo-devtools")())
 }
 
-app.use(require('./stores/clicks'))
+app.use(require("./stores/clicks"))
+app.use(function (state, emitter) {
+  state.rowCount = 1000
+  state.colCount = 5
+})
 
-app.route('/', require('./views/landing'))
-app.route('/*', require('./views/404'))
+app.route("/rendering-performance", require("./views/rendering-performance"))
+app.route("/", require("./views/landing"))
+app.route("/*", require("./views/404"))
 
-module.exports = app.mount('body')
+module.exports = app.mount("body")
--- packages/choo-site/views/landing.js
+++ packages/choo-site/views/landing.js
@@ -4,29 +4,39 @@ var TITLE = 'JigarGosar.com'
 
 module.exports = view
 
+function link(href, title) {
+  return html`<a class="link dim gray f6 f5-ns dib mr3" href="${href}" title="${title}">${title}</a>`
+}
+
 function view(state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
   // language=HTML
   return html`
-    <body class="code lh-copy">
-    <main class="pa3 cf center">
-      <nav class="pa3 pa4-ns">
-        <a
-            class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
-            href="#"
-            title="Home"
-        >
-          Jigar Gosar
-        </a>
-        <div class="tc pb3">
-          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
-          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-          <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+<body class="code lh-copy">
+<main class="pa3 cf center">
+  <nav class="pa3 pa4-ns">
+    <a
+        class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
+        href="#"
+        title="Home"
+    >
+      Jigar Gosar
+    </a>
+    <div class="tc pb3">
+      <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+      <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
+      <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+      <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+    </div>
+    <section>
+        <h2>Experiments</h2>
+        <div>
+            ${link(`rendering-performance`, `Rendering Performance`)}
         </div>
-      </nav>
-    </main>
-    </body>
-  `
+    </section>
+  </nav>
+</main>
+</body>
+`
 }
