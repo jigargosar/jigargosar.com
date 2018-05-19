992ee43f2834f99e2fd7d14c6491e321d9e71404
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-19 22:08:47 +0530

    feat(choo-notes): showing only title of notes

--- packages/choo-notes/views/main.js
+++ packages/choo-notes/views/main.js
@@ -5,41 +5,45 @@ const TITLE = 'Choo Notes'
 
 module.exports = view
 
-function renderEditButton(emit, idx) {
+function viewEditButton(emit, idx) {
+  return html`<div class="underline" onclick="${onEditClicked}">edit</div>`
+
   function onEditClicked() {
     emit('notes:edit', idx)
   }
-
-  return html`<div class="underline" onclick="${onEditClicked}">edit</div>`
 }
 
-const viewNote = R.curry(function(emit, note, idx) {
+const viewNote = R.curry(function(emit, totalNotes, note, idx) {
   var noteHtml = R.map(
     para => html`<div class="pb2">${para}</div>`,
     note.body.split(/\r\n|\r|\n/),
   )
 
-  return html`<div class="center ph3-m mb4">
-  <div class="flex f5 mb1 mh2">
+  function renderNoteBody() {
+    return html`<div class="f6 bg-white-80 shadow-1 pa3">
+    <div class="measure-wide">${noteHtml}</div>
+  </div>`
+  }
+
+  //<div className="center ph3-m mb4">
+  return html`<div class="center _ph3-m bg-white-80">
+  <div class="flex f5 _mb1 _mh2 pa2 b--black-10 ${
+    totalNotes - 1 === idx ? '' : 'bb'
+  }">
     <div class="flex-grow-1">
       ${note.title}
     </div>
-    ${renderEditButton(emit, idx)}
+    ${viewEditButton(emit, idx)}
   </div>
-  <div class="f6 bg-white-80 shadow-1 pa3">
-    <div class="measure-wide">${noteHtml}</div>
-  </div>
+  ${renderNoteBody() && null}
 </div>`
 })
 
 function view(state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
-  function onAddClick() {
-    emit('notes:add')
-  }
-
   var allNotes = state.notes.list
+
   return html`<body class="bg-black-10 black-80 mw7-l center code lh-copy">
 <header class="bg-black-80 white-80">
   <div class="flex items-center pa3">
@@ -52,13 +56,17 @@ function view(state, emit) {
     </div>
   </div>
 </header>
-<div class="mt4">
+<div class="_mt4 ma3-ns shadow-1">
   ${R.addIndex(R.map)(
-    viewNote(emit),
+    viewNote(emit, allNotes.length),
     // R.sortWith([R.descend(R.prop('modifiedAt'))], allNotes),
     allNotes,
   )}
 </div>
 </body>      
-  `
+`
+
+  function onAddClick() {
+    emit('notes:add')
+  }
 }
