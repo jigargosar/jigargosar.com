f14cd3d530f1d50a8cea8272d1fbb141f6043686
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-19 18:40:41 +0530

    feat(choo-notes): show fake notes list

--- packages/choo-notes/stores/notes.js
+++ packages/choo-notes/stores/notes.js
@@ -1,11 +1,33 @@
+var R = require('ramda')
+
 module.exports = store
+var faker = require('faker')
+var Chance = require('chance')
 
-function store (state, emitter) {
-  state.totalClicks = 0
+var chance = new Chance(123)
 
-  emitter.on('DOMContentLoaded', function () {
-    emitter.on('clicks:add', function (count) {
-      state.totalClicks += count
+function createFakeNote() {
+  return {
+    text: faker.lorem.paragraphs(),
+  }
+}
+
+const fakeNotes = R.times(
+  createFakeNote,
+  chance.natural({
+    min: 3,
+    max: 10,
+  }),
+)
+
+function store(state, emitter) {
+  state.notes = {
+    list: fakeNotes,
+  }
+
+  emitter.on('DOMContentLoaded', function() {
+    emitter.on('notes:add', function() {
+      state.notes.list.push(createFakeNote())
       emitter.emit(state.events.RENDER)
     })
   })
--- packages/choo-notes/views/main.js
+++ packages/choo-notes/views/main.js
@@ -1,19 +1,36 @@
-var html = require("choo/html")
+const R = require('ramda')
+const html = require('choo/html')
 
-var TITLE = "choo-notes - main"
+const TITLE = 'Choo Notes'
 
 module.exports = view
+
+function viewNote(note) {
+  return html`<div class="mb2"><pre>${note.text}</pre></div>`
+}
 
 function view(state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
+  function onClick() {
+    emit('notes.addNew')
+  }
+
+  var allNotes = state.notes.list
   return html`
   <body class="code lh-copy pa3">
-    <div class="flex bb">
-      <div class="f3">Notes List</div>
+    <div class="flex bb items-center">
+      <div class="flex-auto f3">
+        Choo Notes<span class="f6">(${allNotes.length})</span>
+      </div>
+      <button 
+        onclick=${onClick}>
+        Add Note
+      </button>
     </div>
-    
+    <div>
+      ${R.map(viewNote, allNotes)}
+    </div>
   </body>      
   `
-
 }
