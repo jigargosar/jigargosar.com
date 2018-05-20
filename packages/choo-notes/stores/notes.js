b936b4c07e3940518de12d0cedf5e94735a45700
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-20 11:09:41 +0530

    feat(choo-notes): using state transitions and actions to move to editing state

--- packages/choo-notes/stores/notes.js
+++ packages/choo-notes/stores/notes.js
@@ -16,33 +16,39 @@ function createFakeNote({modifiedAt} = {}) {
 const fakeNotes = R.times(createFakeNote, 15)
 
 function store(state, emitter) {
+  const viewMachine = Machine({
+    key: 'notesViewState',
+    initial: 'list',
+    strict: true,
+    states: {
+      list: {
+        on: {
+          addNew: {
+            editing: {
+              actions: [],
+            },
+          },
+          editExisting: {
+            editing: {
+              actions: ['updateNoteAtIndex'],
+            },
+          },
+        },
+      },
+      editing: {
+        on: {
+          save: 'list',
+          cancel: 'list',
+        },
+      },
+      stop: {},
+    },
+  })
+
   state.notes = {
     list: fakeNotes,
-    viewMachine: Machine({
-      key: 'notesViewState',
-      initial: 'list',
-      states: {
-        list: {
-          on: {
-            addNew: 'editNew',
-            editExisting: 'editExisting',
-          },
-        },
-        editNew: {
-          on: {
-            save: 'list',
-            cancel: 'list',
-          },
-        },
-        editExisting: {
-          on: {
-            save: 'list',
-            cancel: 'list',
-          },
-        },
-        stop: {},
-      },
-    }),
+    viewMachine: viewMachine,
+    currentState: viewMachine.initialState,
   }
 
   function render() {
@@ -56,14 +62,24 @@ function store(state, emitter) {
     })
 
     emitter.on('notes:edit', function(idx) {
-      state.notes.list = R.update(
-        idx,
-        createFakeNote({modifiedAt: Date.now()}),
-      )(state.notes.list)
-      state.notes.viewMachine.transition(
-        state.notes.viewMachine.state,
-        'editExisting',
+      state.notes.currentState = viewMachine.transition(
+        state.notes.currentState,
+        {type: 'editExisting', idx},
       )
+      const actionMap = {
+        updateNoteAtIndex: function() {
+          state.notes.editing = {idx}
+          state.notes.list = R.update(
+            idx,
+            createFakeNote({modifiedAt: Date.now()}),
+          )(state.notes.list)
+        },
+      }
+
+      R.forEach(action =>
+        R.propOr(R.T, action, actionMap)(),
+      )(state.notes.currentState.actions)
+
       render()
     })
   })
