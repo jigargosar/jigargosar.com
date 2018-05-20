c63ca6bfa4463afb6c96bfd04abedcf6e04894ea
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-20 13:07:36 +0530

    feat(choo-notes): validating transition

--- packages/choo-notes/stores/notes.js
+++ packages/choo-notes/stores/notes.js
@@ -1,6 +1,7 @@
 const Machine = require('xstate').Machine
 const R = require('ramda')
 const faker = require('faker')
+const assert = require('assert')
 faker.seed(123)
 
 module.exports = store
@@ -15,70 +16,81 @@ function createFakeNote({modifiedAt} = {}) {
 
 const fakeNotes = R.times(createFakeNote, 15)
 
-function store(state, emitter) {
-  const viewMachine = Machine({
-    key: 'notesViewState',
-    initial: 'list',
-    strict: true,
-    states: {
-      list: {
-        on: {
-          addNew: {
-            editing: {
-              actions: [],
-            },
+const viewMachine = Machine({
+  key: 'notesViewState',
+  initial: 'list',
+  strict: true,
+  states: {
+    list: {
+      on: {
+        addClicked: {
+          editing: {
+            actions: ['addNewNote'],
           },
-          editExisting: {
-            editing: {
-              actions: ['updateNoteAtIndex'],
-            },
+        },
+        editClicked: {
+          editing: {
+            actions: ['updateNoteAtIndex'],
           },
         },
       },
-      editing: {
-        on: {
-          save: 'list',
-          cancel: 'list',
-        },
-      },
-      stop: {},
     },
-  })
+    editing: {
+      on: {
+        save: 'list',
+        cancel: 'list',
+      },
+    },
+    stop: {},
+  },
+})
 
-  state.notes = {
+const performAction = R.curry((actionMap, event, actionName) =>
+  R.propOr(R.T, actionName, actionMap)(event),
+)
+
+function store(state, emitter) {
+  const notes = {
     list: fakeNotes,
     viewMachine: viewMachine,
-    currentState: viewMachine.initialState,
+    viewState: viewMachine.initialState,
   }
 
+  state.notes = notes
   function render() {
     emitter.emit(state.events.RENDER)
   }
 
-  emitter.on('DOMContentLoaded', function() {
-    emitter.on('notes:add', function() {
-      state.notes.list.unshift(createFakeNote({modifiedAt: Date.now()}))
+  const actionMap = {
+    updateNoteAtIndex: function({idx}) {
+      notes.editing = {idx}
+      notes.list = R.update(idx, createFakeNote({modifiedAt: Date.now()}))(
+        notes.list,
+      )
+    },
+  }
+  function getViewStateValue() {
+    return notes.viewState.value || viewMachine.initialStateValue
+  }
+  emitter.on('DOMContentLoaded', () => {
+    emitter.on('notes:add', () => {
+      notes.list.unshift(createFakeNote({modifiedAt: Date.now()}))
       render()
     })
-
-    emitter.on('notes:edit', function(idx) {
-      state.notes.currentState = viewMachine.transition(
-        state.notes.currentState,
-        {type: 'editExisting', idx},
-      )
-      const actionMap = {
-        updateNoteAtIndex: function() {
-          state.notes.editing = {idx}
-          state.notes.list = R.update(
-            idx,
-            createFakeNote({modifiedAt: Date.now()}),
-          )(state.notes.list)
-        },
+    emitter.on('notes:edit', idx => {
+      const event = {
+        type: 'editClicked',
+        idx,
       }
+      const nextViewState = viewMachine.transition(notes.viewState.value, event)
 
-      R.forEach(action =>
-        R.propOr(R.T, action, actionMap)(),
-      )(state.notes.currentState.actions)
+      assert.notEqual(
+        notes.viewState.value,
+        nextViewState.value,
+        `Action 'notes:edit', Transition failed for event ${event}`,
+      )
+      notes.viewState = nextViewState
+      notes.viewState.actions.forEach(performAction(actionMap, event))
 
       render()
     })
