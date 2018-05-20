fafe698d98f95b84a3afea19e232ec730267c9bf
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-20 18:16:22 +0530

    feat(idLookup): added id to createFakeNote

--- packages/choo-notes/models/note.js
+++ packages/choo-notes/models/note.js
@@ -1,12 +1,14 @@
 const faker = require('faker')
 faker.seed(123)
 
-exports.createFakeNote = function createFakeNote({
-  /*modifiedAt*/
-} = {}) {
-  return {
+let nextId = 1
+
+exports.createFakeNote = function createFakeNote() {
+  const note = {
+    id: `${nextId}`,
     title: faker.lorem.words(),
     body: faker.lorem.text(),
-    // modifiedAt: modifiedAt || faker.date.recent(),
   }
+  nextId++
+  return note
 }
