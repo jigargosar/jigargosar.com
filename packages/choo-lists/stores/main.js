c9e9a2e2ee1e0875be19b67cce7c986f44f47eca
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-22 13:08:23 +0530

    proto(choo-lists): list items

--- packages/choo-lists/stores/main.js
+++ packages/choo-lists/stores/main.js
@@ -1,12 +1,22 @@
-module.exports = store;
+const R = require('ramda')
+const log = require('nanologger')('store:main')
+
+module.exports = store
 
 function store(state, emitter) {
-  state.list = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+  state.list = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+  state.events.list_add = 'list:add'
+  state.events.list_delete = 'list:delete'
+  emitter.on('DOMContentLoaded', function() {
+    const eventMap = {
+      [state.events.list_add]: function(text) {
+        state.list.prepend(text)
+      },
+      [state.events.list_delete]: function(idx) {
+        state.list.splice(idx, 1)
+      },
+    }
 
-  emitter.on("DOMContentLoaded", function() {
-    emitter.on("clicks:add", function(count) {
-      state.totalClicks += count;
-      emitter.emit(state.events.RENDER);
-    });
-  });
+    R.forEachObjIndexed((fn, name) => emitter.on(name, fn))(eventMap)
+  })
 }
--- packages/choo-lists/views/main.js
+++ packages/choo-lists/views/main.js
@@ -3,6 +3,7 @@ const html = require('choo/html')
 
 const {TITLE} = require('./meta')
 const {updateTitle} = require('./events')
+const I = require('../models/item')
 
 const style = css`
   :host {
@@ -17,7 +18,18 @@ function view(state, emit) {
   return html`
 <body class="f4">
   <h1 class="bg-blue-light text-center ${style}">${TITLE}</h1>
-  <div class="container-md p-4">Content</div>
+  <div class="f2 container-md p-4">Items</div>
+  <div class="container-md">${viewItemsList(state, emit)}</div>
 </body>
+`
+}
+
+function viewItemsList(state, emit) {
+  return html`
+<div class="">
+  ${state.list.map(function(item) {
+    return html`<div class="px-4 py-1">${I.text(item)} </div>`
+  })}  
+</div>
 `
 }
