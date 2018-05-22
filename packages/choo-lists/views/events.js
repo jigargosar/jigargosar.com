6c809dc253e095dd0a3d84ac32a3ba242bd69cc6
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-22 12:35:26 +0530

    proto(choo-lists): extract title code

---
+++ packages/choo-lists/views/events.js
@@ -0,0 +1,5 @@
+const R = require('ramda')
+
+export const updateTitle = R.curry(function updateTitle(TITLE, state, emit) {
+  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+})
--- packages/choo-lists/views/main.js
+++ packages/choo-lists/views/main.js
@@ -1,9 +1,10 @@
-var css = require('sheetify')
-var html = require('choo/html')
+const css = require('sheetify')
+const html = require('choo/html')
 
-var TITLE = 'Choo List Prototype'
+const {TITLE} = require('./meta')
+const {updateTitle} = require('./events')
 
-var style = css`
+const style = css`
   :host {
     font-size: 4rem;
   }
@@ -11,11 +12,11 @@ var style = css`
 module.exports = view
 
 function view(state, emit) {
-  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  updateTitle(TITLE, state, emit)
 
   return html`
 <body class="f4">
-  <h1 class="bg-blue-light text-center ${style}">Choo List Prototype</h1>
+  <h1 class="bg-blue-light text-center ${style}">${TITLE}</h1>
   <div class="container-md p-4">Content</div>
 </body>
 `
