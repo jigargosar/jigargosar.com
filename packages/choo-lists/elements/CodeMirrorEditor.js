457366be4316dd2d0a9b73e65f028e96bea195de
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-26 15:30:47 +0530

    proto(choo-lists): added linter for YAML

--- packages/choo-lists/elements/CodeMirrorEditor.js
+++ packages/choo-lists/elements/CodeMirrorEditor.js
@@ -3,14 +3,27 @@ const CodeMirror = require('codemirror')
 const Component = require('nanocomponent')
 const html = require('choo/html')
 const css = require('sheetify')
-
+// require('codemirror/addon/lint/lint')
+// require('codemirror/mode/gfm/gfm')
+// require('codemirror/mode/markdown/markdown')
+// require('codemirror/mode/yaml-frontmatter/yaml-frontmatter')
+require('codemirror/mode/yaml/yaml')
+window.jsyaml = require('js-yaml')
+require('codemirror/addon/lint/lint')
+require('codemirror/addon/lint/yaml-lint')
 css('codemirror/lib/codemirror.css')
-css('codemirror/theme/zenburn.css')
+css('codemirror/addon/lint/lint.css')
+css('codemirror/theme/blackboard.css')
 
 class CodeMirrorEditor extends Component {
   load() {
     this._cm = CodeMirror.fromTextArea(this.element.firstChild, {
       lineNumbers: true,
+      // theme: 'default',
+      theme: 'blackboard',
+      mode: 'yaml',
+      gutters: ['CodeMirror-lint-markers'],
+      lint: true,
     })
     this._cm.setSize(null, 200)
   }
