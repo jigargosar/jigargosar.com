722a313a0933cdafec0121afc0e38d207e24ff58
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-27 17:18:14 +0530

    proto(cra-lists): basic layout styling

--- packages/cra-lists/src/App.js
+++ packages/cra-lists/src/App.js
@@ -1,16 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">CRA List Prototype</h1>
+      <div className="flex-grow-1 flex flex-column sans-serif bg-black-05">
+        <header className="tc bg-black white pa3">
+          <div className="f1">CRA List Prototype</div>
         </header>
-        <p className="App-intro">
+        <main className="flex-grow-1 bg-white center w-100 mw7 pa3">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </main>
       </div>
     );
   }
