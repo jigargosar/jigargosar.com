bd1e09a8ae55fa972c8878c5178e0d9dcca143d4
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-27 15:16:29 +0530

    proto(choo-lists): bankai build failing this is BS

--- packages/choo-lists/index.js
+++ packages/choo-lists/index.js
@@ -1,28 +1,39 @@
-require('babel-polyfill')
-const log = require('nanologger')('app')
-const css = require('sheetify')
-css('tachyons')
+// require('babel-polyfill')
 
 const choo = require('choo')
+const html = require('choo/html')
+const css = require('sheetify')
+const NanoLogger = require('nanologger')
+const assert = require('assert')
+
+console.log('foo', ...{a: 1})
+
+css('tachyons')
 const app = choo()
 
-window.addEventListener('error', function(event) {
-  log.error(event.error.reason, event.error.message)
+var log = new NanoLogger('index')
+
+// window.addEventListener('error', function(event) {
+//   log.error(event.error.reason, event.error.message)
+// })
+//
+// window.addEventListener('unhandledrejection', function(event) {
+//   log.error(event.error.reason, event.error.message)
+// })
+
+// if (process.env.NODE_ENV !== 'production') {
+//   app.use(require('choo-devtools')())
+// } else {
+//   app.use(require('choo-service-worker')())
+// }
+
+// app.use(require('./stores/list'))
+
+// app.route('/', require('./views/list'))
+app.route('/', function() {
+  return html`<body>HW</body>`
 })
 
-window.addEventListener('unhandledrejection', function(event) {
-  log.error(event.error.reason, event.error.message)
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
-}
-
-app.use(require('./stores/list'))
-
-app.route('/', require('./views/list'))
 app.route('/*', require('./views/404'))
 
 module.exports = app.mount('body')
