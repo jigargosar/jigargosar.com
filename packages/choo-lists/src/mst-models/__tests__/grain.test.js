12cbc62403989d1a7cac9ea5c3128241b1c23897
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-02 23:32:30 +0530

    proto(choo-lists): optimize imports

---
+++ packages/choo-lists/src/mst-models/__tests__/grain.test.js
@@ -0,0 +1,27 @@
+import {Grain} from '../grain'
+
+const log = require('nanologger')('grain.test')
+
+// noinspection SpellCheckingInspection
+jest.mock('nanoid', () => jest.fn(() => 'BitYkpcz~i4qTWDlMgGbS'))
+jest.spyOn(Date, 'now').mockImplementation(() => 1527958482904)
+
+beforeEach(() => {
+  jest.resetModules()
+})
+
+describe('Grain', function() {
+  test('should get created without any arguments', function() {
+    const grain = Grain.create()
+    expect(grain).toMatchSnapshot()
+    expect(grain.getText()).toEqual('')
+    grain.userPatchProps({text: 'foo'})
+    expect(grain.getText()).toEqual('foo')
+  })
+  test('should have text prop', function() {
+    const grain = Grain.create()
+    expect(grain.getText()).toEqual('')
+    grain.userPatchProps({text: 'foo'})
+    expect(grain.getText()).toEqual('foo')
+  })
+})
--- packages/choo-lists/src/mst-models/grain.js
+++ packages/choo-lists/src/mst-models/grain.js
@@ -1,46 +1,26 @@
+import {BaseModel} from './base-model'
+import {types} from 'mobx-state-tree'
+
 const R = require('ramda')
 const RA = require('ramda-adjunct')
 
-import {types} from 'mobx-state-tree'
-import {nanoId, timestamp} from '../mst-types'
-
 const assert = require('assert')
 
-export const BaseModel = types
-  .model('Grain', {
-    id: nanoId,
-    createdAt: timestamp,
-    modifiedAt: timestamp,
-  })
-  .actions(self => ({
-    afterCreate() {
-      assert(RA.isArray(self.getUserEditableProps()))
-    },
-    userPatchProps(props) {
-      assert(R.compose(R.isEmpty, R.omit(self.getUserEditableProps()))(props))
-      Object.assign(self, R.pick(self.userEditableProps, props))
-    },
-  }))
-
 export const Grain = BaseModel.named('Grain')
-  .props('Grain', {
-    text: types.string,
+  .props({
+    text: types.optional(types.string, () => ''),
   })
   .views(self => ({
     getText() {
       return self.text
     },
-
     getUserEditableProps() {
       return ['text']
     },
   }))
-  .actions(self => ({
-    getText() {
-      return self.text
-    },
 
-    getUserEditableProps() {
-      return ['text']
-    },
-  }))
+// .actions(self => ({
+//   setText(text){
+//     return self.text = text
+//   }
+// }))
