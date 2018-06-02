f8d4888171ba4ad4b722eda0e04186a68a449be6
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-02 23:47:25 +0530

    proto(choo-lists): added views and actions to GS

--- packages/choo-lists/src/mst-models/grain.js
+++ packages/choo-lists/src/mst-models/grain.js
@@ -25,6 +25,17 @@ export const Grain = BaseModel.named('Grain')
 //   }
 // }))
 
-export const GrainStore = types.model('GrainStore', {
-  modelMap: types.optional(types.map(Grain), () => ({})),
-})
+export const GrainStore = types
+  .model('GrainStore', {
+    modelMap: types.optional(types.map(Grain), () => ({})),
+  })
+  .views(self => ({
+    getList() {
+      return Array.from(self.modelMap.values())
+    },
+  }))
+  .actions(self => ({
+    add(model) {
+      self.modelMap.put(model)
+    },
+  }))
