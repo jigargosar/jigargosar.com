2a12bc121ab1f8bf2f339afef50bcaa6280ff8ce
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-08 10:10:20 +0530

    proto(cra-lists): inject

--- packages/cra-lists/src/App.js
+++ packages/cra-lists/src/App.js
@@ -1,19 +1,24 @@
 import React, {Component, Fragment as F} from 'react'
+import {inject, observer} from 'mobx-react'
+
+const R = require('ramda')
+const RA = require('ramda-adjunct')
 
 const centeredContentClass = 'center mw7 mv3 ph3'
-class App extends Component {
-  render() {
-    const TITLE = 'CRA List Prototype'
-    return (
-      <F>
-        <div className="bg-light-blue tc pa3">
-          <div className="f1">{TITLE}</div>
-          {/*<div>${signInOutView(state, emit)}</div>*/}
-        </div>
-        <div className={`flex ${centeredContentClass}`}>LIST</div>
-      </F>
-    )
-  }
-}
-
+const App = R.compose(inject('root'), observer)(
+  class App extends Component {
+    render() {
+      const {pageTitle} = this.props.root
+      return (
+        <F>
+          <div className="bg-light-blue tc pa3">
+            <div className="f1">{pageTitle}</div>
+            {/*<div>${signInOutView(state, emit)}</div>*/}
+          </div>
+          <div className={`flex ${centeredContentClass}`}>LIST</div>
+        </F>
+      )
+    }
+  },
+)
 export default App
