12c19b20b74cb1b12328c3733712405661a37658
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-08 10:22:42 +0530

    proto(cra-lists): HMR

--- packages/cra-lists/src/index.js
+++ packages/cra-lists/src/index.js
@@ -6,10 +6,22 @@ import registerServiceWorker from './registerServiceWorker'
 import {Provider} from 'mobx-react'
 import {rootStore} from './stores/rootStore'
 
-ReactDOM.render(
-  <Provider root={rootStore}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-)
+function render(App) {
+  ReactDOM.render(
+    <Provider root={rootStore}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const App = require('./App').default
+    render(App)
+  })
+}
+
 registerServiceWorker()
