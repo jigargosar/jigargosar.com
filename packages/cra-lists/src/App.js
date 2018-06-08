7cf9ea007b683108d7f21a3f2f63302e06e0f4a5
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-08 17:09:52 +0530

    proto(cra-lists): number list

--- packages/cra-lists/src/App.js
+++ packages/cra-lists/src/App.js
@@ -9,7 +9,7 @@ const injectS = R.compose(inject('s'), observer)
 
 const Counter = injectS(function Counter({s}) {
   return (
-    <div className={`flex flex-column ${centeredContentClass}`}>
+    <div className={`${centeredContentClass}`}>
       <div>Counter : {s.counter}</div>
       <div>
         <button onClick={() => s.inc(1)}>+</button>
@@ -20,17 +20,39 @@ const Counter = injectS(function Counter({s}) {
   )
 })
 
-function App({s}) {
+const Header = injectS(function Header({s}) {
+  return (
+    <div className="bg-light-blue tc pa3">
+      <div className="f1">{s.pageTitle}</div>
+      {/*<div>${signInOutView(state, emit)}</div>*/}
+    </div>
+  )
+})
+
+const GrainItem = injectS(function GrainItem({item}) {
+  return <div>{item}</div>
+})
+
+const GrainsList = injectS(function GrainsList() {
+  const list = [1, 2, 3]
+  return (
+    <div>
+      {R.map(item => <GrainItem key={item} item={item} />)(list)}
+    </div>
+  )
+})
+
+const App = injectS(function App() {
   return (
     <F>
-      <div className="bg-light-blue tc pa3">
-        <div className="f1">{s.pageTitle}</div>
-        {/*<div>${signInOutView(state, emit)}</div>*/}
+      <Header />
+      <div className={`${centeredContentClass}`}>
+        <div>LIST</div>
+        <GrainsList />
       </div>
-      <div className={`flex ${centeredContentClass}`}>LIST</div>
       <Counter />
     </F>
   )
-}
+})
 
-export default injectS(App)
+export default App
