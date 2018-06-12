6dc3578e38fc4311d0557130e22efaab20e236e5
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-12 10:17:37 +0530

    proto(cra-lists): switching to web module

--- packages/cra-lists/src/mobx-stores/pouch-db-store.js
+++ packages/cra-lists/src/mobx-stores/pouch-db-store.js
@@ -1,5 +1,7 @@
 import PouchDB from 'pouchdb-browser'
+import 'pouchdb-all-dbs'
 import ow from 'ow'
 export function PouchCollectionStore(dbName) {
   ow(dbName, ow.string.minLength(3))
+  const db = new PouchDB(dbName)
 }
