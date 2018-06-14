ebb1437c07d79d6b23d588ddddaca1cc2ec1ed5d
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-14 09:39:26 +0530

    add firebase sync

--- packages/cra-lists/src/mobx-stores/FirePouchSync.js
+++ packages/cra-lists/src/mobx-stores/FirePouchSync.js
@@ -1,37 +1,54 @@
 import {FirebaseStore} from './FirebaseStore'
-import {observable, reaction, runInAction} from 'mobx'
+import {observable, reaction, action} from 'mobx'
 const firebase = require('firebase/app')
 const Timestamp = firebase.firestore.Timestamp
 
 export function FirePouchSync(pc) {
   const fireSync = observable(
-    {syncMilli: 0, syncSeq: 0, syncing: false},
-    {},
+    {
+      syncMilli: 0,
+      syncSeq: 0,
+      syncing: false,
+      fireQueue: [],
+      pouchQueue: [],
+      queueFireSnapshot(snapshot) {
+        this.fireQueue.push(snapshot)
+      },
+      queuePouchChange(change) {
+        this.pouchQueue.push(change)
+      },
+    },
+    {queueFireSnapshot: action.bound, queuePouchChange: action.bound},
     {name: `PouchFireSync: ${pc.name}`},
   )
   reaction(
     () => [FirebaseStore.user, fireSync.syncing],
     async () => {
       if (FirebaseStore.user && !fireSync.syncing) {
-        fireSync.syncing = true
+        let snapshotDisposer, changes
         try {
+          fireSync.syncing = true
           const cRef = FirebaseStore.getUserCollectionRef(pc.name)
-          const qs = await cRef
+          snapshotDisposer = await cRef
             .where(
               'serverTimestamp',
               '>',
               Timestamp.fromMillis(fireSync.syncMilli),
             )
-            .get()
-          console.log('qs.metadata', qs.metadata)
-          console.log('qs.docs', qs.docs, qs.docChanges())
+            .onSnapshot(snapshot => fireSync.queueFireSnapshot)
 
-          const pChanges = await pc.pouchStore.changes({
-            since: fireSync.syncSeq,
-          })
-          console.log('pc.pouchStore.changes', pChanges)
+          changes = await pc.pouchStore
+            .liveChanges({
+              since: fireSync.syncSeq,
+            })
+            .on('change', fireSync.queuePouchChange)
+        } catch (e) {
+          console.error(e)
+          if (snapshotDisposer) snapshotDisposer()
+          if (changes) changes.cancel()
+          // runInAction(() => (fireSync.syncing = false))
         } finally {
-          runInAction(() => (fireSync.syncing = true))
+          // runInAction(() => (fireSync.syncing = true))
         }
       }
     },
