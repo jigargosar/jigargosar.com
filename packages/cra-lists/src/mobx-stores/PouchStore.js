12d73d21bd2075fdbbef89cd63f0824c4325df93
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-14 09:14:26 +0530

    add firebase sync

--- packages/cra-lists/src/mobx-stores/PouchStore.js
+++ packages/cra-lists/src/mobx-stores/PouchStore.js
@@ -29,11 +29,12 @@ export function PouchStore(db, service) {
     liveChanges({since}) {
       return this.changes({since, live: true})
     },
-    changes({since = 0, live = false} = {}) {
+    changes({since = 0, live = false, ...rest} = {}) {
       const changesOptions = {
         include_docs: true,
         since,
         live: live,
+        ...rest,
       }
       const changes = db.changes(changesOptions)
       changes.on('error', e =>
