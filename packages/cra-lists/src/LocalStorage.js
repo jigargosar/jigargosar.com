690a2293a9a038b4dcc05e66a516ec98c6acff48
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-14 12:42:12 +0530

    add firebase sync

--- packages/cra-lists/src/LocalStorage.js
+++ packages/cra-lists/src/LocalStorage.js
@@ -6,7 +6,7 @@ function getOrSetLocalStorage(id, setValue) {
   if (R.isNil(value)) {
     value = window.localStorage.setItem(id, setValue)
   }
-  return value
+  return JSON.parse(value)
 }
 
 export function getAppActorId() {
