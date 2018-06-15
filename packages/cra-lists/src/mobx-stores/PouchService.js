f3de672f30f652f2530d64f043ef90805d3029a5
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-15 12:35:09 +0530

    cleanup

--- packages/cra-lists/src/mobx-stores/PouchService.js
+++ packages/cra-lists/src/mobx-stores/PouchService.js
@@ -1,6 +1,5 @@
 import PouchDB from 'pouchdb-browser'
 import ow from 'ow'
-import {PouchStore} from './PouchStore'
 
 const assert = require('assert')
 
@@ -32,4 +31,52 @@ if (module.hot) {
   window.requestAnimationFrame(() =>
     PouchService.getAllDbNames().then(console.log),
   )
+}
+
+// import nanoid from 'nanoid'
+const R = require('ramda')
+
+function PouchStore(db, service) {
+  return {
+    _db: db,
+    get name() {
+      return db.name
+    },
+    destroy() {
+      return service.destroy(db.name)
+    },
+    get(id) {
+      return db.get(id)
+    },
+    put(doc) {
+      return db.put(doc)
+    },
+    allDocs() {
+      return db.allDocs({include_docs: true})
+    },
+    async getAll() {
+      const res = await this.allDocs()
+      return R.map(R.prop('doc'), res.rows)
+    },
+    allChanges() {
+      return db.changes({include_docs: true})
+    },
+    liveChanges({since}) {
+      console.log(this.name, 'liveChanges: since', since)
+      return this.changes({since, live: true})
+    },
+    changes({since = 0, live = false, ...rest} = {}) {
+      const changesOptions = {
+        include_docs: true,
+        since,
+        live: live,
+        ...rest,
+      }
+      const changes = db.changes(changesOptions)
+      changes.on('error', e =>
+        console.error('changes:', this.name, changesOptions, e),
+      )
+      return changes
+    },
+  }
 }
