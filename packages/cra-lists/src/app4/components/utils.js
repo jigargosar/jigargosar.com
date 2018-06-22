b485d865e7bcda023abc511d2e59c5df1566f375
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-22 16:20:33 +0530

    App4: from scratch

--- packages/cra-lists/src/app4/components/utils.js
+++ packages/cra-lists/src/app4/components/utils.js
@@ -2,7 +2,7 @@
 
 /*eslint-disable*/
 import React from 'react'
-import {observer} from 'mobx-react'
+import {observer, Observer} from 'mobx-react'
 
 const R = require('ramda')
 const RA = require('ramda-adjunct')
@@ -20,4 +20,72 @@ export function renderKeyedById(Component, propName, idList) {
   ))(idList)
 }
 
-export const ro = observer
+// export const ro = observer
+
+const StateContext = React.createContext(null)
+
+export const StateProvider = StateContext.Provider
+
+export const withState = c => {
+  return R.compose(
+    BaseComponent =>
+      function StateInjector(props) {
+        return (
+          <StateContext.Consumer>
+            {state => (
+              <BaseComponent
+                {...{
+                  s: state,
+                  fire: state.fire,
+                  auth: state.fire.auth,
+                }}
+                {...props}
+              />
+            )}
+          </StateContext.Consumer>
+        )
+      },
+    observer,
+  )(c)
+}
+
+export const M = observer(
+  class M extends React.Component {
+    render() {
+      return (
+        <StateContext.Consumer>
+          {state => (
+            <Observer>
+              {() =>
+                this.r({
+                  s: state,
+                  fire: state.fire,
+                  auth: state.fire.auth,
+                  ns: state.ns,
+                  ...this.props,
+                })
+              }
+            </Observer>
+          )}
+        </StateContext.Consumer>
+      )
+    }
+
+    r(props) {
+      return props.render ? props.render(this.props) : null
+    }
+  },
+)
+
+export class C extends React.Component {
+  render() {
+    return this.r(this.props)
+  }
+
+  r(props) {
+    return props.render ? props.render(this.props) : null
+  }
+}
+
+export {Observer as O}
+export {observer as o}
--- packages/cra-lists/src/app4/mobx/NotesCollection.js
+++ packages/cra-lists/src/app4/mobx/NotesCollection.js
@@ -1,5 +1,10 @@
-import {oObject} from './utils'
+import {t} from './utils'
 
-export function NotesCollection() {
-  return oObject({notes: []})
-}
+export const Note = t.model({
+  id: t.identifier(t.string),
+  text: t.string,
+})
+
+export const NotesCollection = t.model({
+  idLookup: t.optional(t.map(Note), {}),
+})
