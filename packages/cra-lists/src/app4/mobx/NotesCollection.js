0488571dd38002827c889f28090436fc2b680f02
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-24 07:47:05 +0530

    App4

--- packages/cra-lists/src/app4/mobx/NotesCollection.js
+++ packages/cra-lists/src/app4/mobx/NotesCollection.js
@@ -42,7 +42,6 @@ export const NotesCollection = (function NotesCollection() {
       add(note) {
         this.put(note)
       },
-
       addNewNote() {
         this.add(this.newNote())
       },
