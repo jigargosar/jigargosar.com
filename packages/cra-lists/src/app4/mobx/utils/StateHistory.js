987e74f9f24c0d979e778311e95b3d1254abab08
Jigar Gosar <jigar.gosar@gmail.com>
2018-06-28 12:07:33 +0530

    app4 : using mobx store app structure

--- packages/cra-lists/src/app4/mobx/utils/StateHistory.js
+++ packages/cra-lists/src/app4/mobx/utils/StateHistory.js
@@ -1,34 +1,27 @@
 import {mJS, mReaction, mSpy, oArray} from '../utils'
 
 export function createObservableHistory(obs) {
-  /*const historyList = oArray([mJS(obs)], {name: 'historyList'})
-
-  mReaction(
-    () => mJS(obs),
-    newState => {
-      historyList.push(newState)
-      console.log(`historyList.length`, historyList.length)
-    },
-    {name: 'StateHistory'},
-  )
-
-  return historyList*/
+  // /*const historyList = oArray([mJS(obs)], {name: 'historyList'})
+  //
+  // mReaction(
+  //   () => mJS(obs),
+  //   newState => {
+  //     historyList.push(newState)
+  //     console.log(`historyList.length`, historyList.length)
+  //   },
+  //   {name: 'StateHistory'},
+  // )
+  //
+  // return historyList*/
 }
-let count = 0
-let zeroEvent = null
 mSpy(change => {
   if (change.spyReportStart) {
-    if (count === 0) {
-      zeroEvent = change
-    }
-    count += 1
+    console.groupCollapsed(change.type, change.name)
+    console.log(`change`, change)
   } else if (change.spyReportEnd) {
-    count -= 1
-  }
-
-  if (count === 0) {
-    console.log(`zeroEvent`, zeroEvent)
+    console.groupEnd()
+    // count -= 1
+  } else {
     console.log(`change`, change)
   }
-  console.assert(count >= 0, 'count >=0')
 })
