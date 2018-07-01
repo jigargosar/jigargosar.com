2e10e00dbccc5c696b98003a93ac1292743ddddb
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-01 17:44:11 +0530

    app4: adding write file support

--- packages/cra-lists/config-overrides.js
+++ packages/cra-lists/config-overrides.js
@@ -1,10 +1,14 @@
 // const { compose } = require('react-app-rewired');
 const { injectBabelPlugin , compose} = require('react-app-rewired');
 
-const { partial, identity } = require('ramda');
+const { partial, partialRight,identity } = require('ramda');
+
+const reactAppRewireBuildDev = require('react-app-rewire-build-dev');
+
 
 // noinspection ES6ConvertModuleExportToExport
 module.exports =  compose(
   partial(injectBabelPlugin,['babel-plugin-transform-decorators']),
+  partialRight(reactAppRewireBuildDev,[{outputPath:'./dev-build'}])
   // partial(injectBabelPlugin,['babel-plugin-transform-decorators'])
 )
