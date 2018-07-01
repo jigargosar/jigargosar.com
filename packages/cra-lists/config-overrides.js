c104de9e3e1d56bb29b8e3e1b4ac0165e85a032d
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-01 18:04:52 +0530

    app4: using cra-build-watch instead of rewire for writing output during dev

--- packages/cra-lists/config-overrides.js
+++ packages/cra-lists/config-overrides.js
@@ -3,12 +3,12 @@ const { injectBabelPlugin , compose} = require('react-app-rewired');
 
 const { partial, partialRight,identity } = require('ramda');
 
-const reactAppRewireBuildDev = require('react-app-rewire-build-dev');
+// const reactAppRewireBuildDev = require('react-app-rewire-build-dev');
 
 
 // noinspection ES6ConvertModuleExportToExport
 module.exports =  compose(
   partial(injectBabelPlugin,['babel-plugin-transform-decorators']),
-  partialRight(reactAppRewireBuildDev,[{outputPath:'./dev-build'}])
+  // partialRight(reactAppRewireBuildDev,[{outputPath:'./dev-build'}])
   // partial(injectBabelPlugin,['babel-plugin-transform-decorators'])
 )
