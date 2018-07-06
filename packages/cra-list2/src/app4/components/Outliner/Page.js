744c203c7f22fbf27f66c77d10909c8c6e86d450
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-06 13:18:21 +0530

    cra-list2: gun outliner

--- packages/cra-list2/src/app4/components/Outliner/Page.js
+++ packages/cra-list2/src/app4/components/Outliner/Page.js
@@ -1,11 +1,45 @@
 import React from 'react'
-import {cn, mrInjectAll} from '../utils'
+import {cn, F, mrInjectAll, renderKeyedById} from '../utils'
+import {Text} from '../ui'
 
-const OutlinePage = mrInjectAll(function App({out}) {
+const out = {
+  root: {
+    id: '0',
+    text: 'Home',
+    lines: [
+      {
+        id: '1',
+        text: 'line 1',
+        lines: [],
+      },
+      {
+        id: '2',
+        text: 'line 2',
+        lines: [],
+      },
+    ],
+  },
+}
+
+function Outline({line}) {
   return (
-    <div className={cn('pa3 bg-blue white shadow-1')}>
-      {`Outliner: ${out}`}
+    <div className={cn('pa1')}>
+      <Text>{line.text}</Text>
+      {renderKeyedById(Outline, 'line', line.lines)}
     </div>
+  )
+}
+
+const OutlinePage = mrInjectAll(function App() {
+  return (
+    <F>
+      <div className={cn('pa3 bg-blue white shadow-1')}>
+        <Text>{`Outliner`}</Text>
+      </div>
+      <div className={cn('pa3')}>
+        <Outline line={out.root} />
+      </div>
+    </F>
   )
 })
 
