4a2cedea37fa922f73ebf5eec110a66872eba046
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-06 13:35:37 +0530

    cra-list2: gun outliner

--- packages/cra-list2/src/app4/mobx/Outliner.js
+++ packages/cra-list2/src/app4/mobx/Outliner.js
@@ -1,36 +1,43 @@
 import {createObservableObject} from './utils'
+import {nanoid} from '../model/util'
+import {storage, StorageItem} from '../services/storage'
+
+function Outline({id = nanoid(), text = 'line x', lines = []} = {}) {
+  const line = createObservableObject({
+    props: {
+      id,
+      text,
+      lines: lines.map(Outline),
+    },
+    actions: {},
+    name: 'Outline',
+  })
+
+  return line
+}
+
+const outlineSI = StorageItem({
+  name: 'outliner-root',
+  getInitial() {
+    return Outline({text: 'line1'})
+  },
+  postLoad(root) {},
+})
 
 export function Outliner() {
   const out = createObservableObject({
     props: {
+      root: Outline({
+        text: 'Home',
+        lines: [{text: 'line1'}],
+      }),
       get lines() {
-        return [
-          {
-            id: '1',
-            text: 'line 1 c',
-            lines: [],
-          },
-          {
-            id: '2',
-            text: 'line 2',
-            lines: [
-              {
-                id: '1',
-                text: 'line 2-1',
-                lines: [],
-              },
-              {
-                id: '2',
-                text: 'line 2-2',
-                lines: [],
-              },
-            ],
-          },
-        ]
+        return this.root.lines
       },
     },
     actions: {},
     name: 'Outliner',
   })
+
   return out
 }
