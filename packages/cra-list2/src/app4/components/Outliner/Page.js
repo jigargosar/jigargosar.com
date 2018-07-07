9e731752d5e28e120a55e8c5d215d83ee2d1316e
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-07 09:11:23 +0530

    cra-list2: gun outliner

--- packages/cra-list2/src/app4/components/Outliner/Page.js
+++ packages/cra-list2/src/app4/components/Outliner/Page.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {cn, F, mrInjectAll, observer, renderKeyedById} from '../utils'
+import {
+  cn,
+  F,
+  isAnyHotKey,
+  mrInjectAll,
+  observer,
+  renderKeyedById,
+} from '../utils'
 import {Text} from '../ui'
+import {_} from '../../utils'
 
 const OutLines = observer(function OutLines({lines}) {
   return renderKeyedById(Outline, 'line', lines)
 })
 
-const Outline = observer(function Outline({line}) {
+const Outline = mrInjectAll(function Outline({line, out}) {
   return (
     <div className={cn('flex items-center')}>
       {/*<Text>{line.text}</Text>*/}
@@ -14,6 +22,11 @@ const Outline = observer(function Outline({line}) {
         className={cn('flex-auto pa1 ')}
         value={line.text}
         onChange={e => line.userUpdate({text: e.target.value})}
+        onKeyDown={e => {
+          _.cond([[isAnyHotKey(['Enter'], () => out.onEnter(line))]])(
+            e,
+          )
+        }}
       />
       <OutLines lines={line.lines} />
     </div>
--- packages/cra-list2/src/app4/mobx/Outliner.js
+++ packages/cra-list2/src/app4/mobx/Outliner.js
@@ -1,7 +1,7 @@
 import {createObservableObject, mReaction} from './utils'
 import {nanoid} from '../model/util'
 import {StorageItem} from '../services/storage'
-import {_} from '../utils'
+import {_, validate} from '../utils'
 
 function Outline({id = nanoid(), text = 'line x', lines = []} = {}) {
   const userProps = ['text']
@@ -11,8 +11,43 @@ function Outline({id = nanoid(), text = 'line x', lines = []} = {}) {
       id,
       text,
       lines: lines.map(Outline),
+      findById(id) {
+        if (_.equals(id, this.id)) {
+          return this
+        } else {
+          return _.reduce(
+            (acc, line) =>
+              _.isNil(acc) ? line.findById(id) : _.reduced(acc),
+            null,
+            lines,
+          )
+        }
+      },
+      findChildById(id) {
+        return _.find(_.propEq('id', id), lines)
+      },
+      findParentOfId(id) {
+        if (_.isNil(this.findChildById(id))) {
+          _.reduce(
+            (acc, line) =>
+              _.isNil(acc) ? line.findParentOfId(id) : _.reduced(acc),
+            null,
+            lines,
+          )
+        } else {
+          return this
+        }
+      },
     },
     actions: {
+      insertNewAfter(id) {
+        this.lines.splice(
+          _.indexOf(this.findChildById(id)),
+          0,
+          Outline(),
+        )
+      },
+
       userUpdate(props) {
         Object.assign(this, pickUserProps(props))
       },
@@ -41,7 +76,13 @@ export function Outliner() {
         return this.root.lines
       },
     },
-    actions: {},
+    actions: {
+      onEnter(line) {
+        const parent = this.root.findParentOfId(line.id)
+        validate('O', [parent])
+        parent.insertNewAfter(line.id)
+      },
+    },
     name: 'Outliner',
   })
 
