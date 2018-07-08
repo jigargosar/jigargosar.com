7c7e41867d7b988ee7991200543d258f5a8784a6
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-08 11:31:56 +0530

    cra-list2: upsert

--- packages/cra-list2/src/app4/model/upsert.js
+++ packages/cra-list2/src/app4/model/upsert.js
@@ -1,1 +1,13 @@
-export function upsert() {}
+import {_, validate} from '../utils'
+
+const upsert = _.curry(function upsert(
+  options,
+  objOrArr,
+  collectionArray,
+) {
+  validate('OOA|OAA', [options, objOrArr, collectionArray])
+})
+
+upsert.displayName = 'upsert'
+
+export {upsert}
--- packages/cra-list2/src/app4/model/upsert.spec.js
+++ packages/cra-list2/src/app4/model/upsert.spec.js
@@ -2,6 +2,12 @@ import {upsert} from './upsert'
 
 describe('upsert', function() {
   it('should not throw', function() {
-    upsert()
+    const rc = upsert({}, {name: 'foo'}, [])
+    expect(upsert.displayName).toBe('upsert')
+    expect(upsert.length).toBe(3)
+  })
+  it('should insert obj in empty arr', function() {
+    const rc = upsert({}, {name: 'foo'}, [])
+    // expect(rc).toEqual([{name: 'foo'}])
   })
 })
