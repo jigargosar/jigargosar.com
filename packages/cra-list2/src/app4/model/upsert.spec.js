0623ab67b66e96f0c7a8a78e17f383595641f245
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-08 11:57:17 +0530

    cra-list2: upsert

--- packages/cra-list2/src/app4/model/upsert.spec.js
+++ packages/cra-list2/src/app4/model/upsert.spec.js
@@ -6,8 +6,24 @@ describe('upsert', function() {
     expect(upsert.displayName).toBe('upsert')
     expect(upsert.length).toBe(3)
   })
+  const fooItem = {name: 'foo'}
   it('should insert obj in empty arr', function() {
-    const rc = upsert({}, {name: 'foo'}, [])
+    const rc = upsert({}, fooItem, [])
     expect(rc).toEqual([{name: 'foo'}])
+  })
+
+  it.skip('should apply mapBeforeUpsert on obj', function() {
+    pending('it fails')
+    expect.assertions(2)
+    const rc = upsert(
+      {
+        mapBeforeUpsert: item => {
+          expect(item).toEqual(fooItem)
+        },
+      },
+      fooItem,
+      [],
+    )
+    expect(rc).toEqual([{id: 1, name: 'foo'}])
   })
 })
