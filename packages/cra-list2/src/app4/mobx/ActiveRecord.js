470486c6b1dc30ef0b511dbe353099d2d2264055
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-09 12:53:39 +0530

    cra-list2: ActiveRecord

--- packages/cra-list2/src/app4/mobx/ActiveRecord.js
+++ packages/cra-list2/src/app4/mobx/ActiveRecord.js
@@ -10,43 +10,6 @@ import a from 'nanoassert'
 
 import pluralize from 'pluralize'
 
-// const FieldTypeToDefaultValueLookup = {
-//   string: '',
-//   text: '',
-//   bool: false,
-// }
-// const FieldTypes = _.keys(FieldTypeToDefaultValueLookup)
-//
-// function typeNameToValue(type) {
-//   validate('S', [type])
-//   a(_.contains(type, FieldTypes))
-//   return FieldTypeToDefaultValueLookup[type]
-// }
-//
-// function fieldToProp(field) {
-//   validate('S', [field])
-//   const [name, type] = _.split(':', field)
-//   validate('SS', [name, type])
-//   return {[name]: typeNameToValue(type)}
-// }
-//
-// function fieldsToProps(fields) {
-//   validate('A', [fields])
-//   return _.compose(_.mergeAll, _.map(fieldToProp))(fields)
-// }
-//
-// function fieldToName(field) {
-//   validate('S', [field])
-//   const [name, type] = _.split(':', field)
-//   validate('SS', [name, type])
-//   return name
-// }
-//
-// function fieldsToNames(fields) {
-//   validate('A', [fields])
-//   return _.map(fieldToName, fields)
-// }
-
 export function ActiveRecord({fieldNames, name}) {
   validate('AS', [fieldNames, name])
   const activeRecord = createObservableObject({
@@ -64,6 +27,8 @@ export function ActiveRecord({fieldNames, name}) {
             [propName]: _.defaultTo(null, defaultValues[propName]),
           })),
         )(fieldNames)
+
+        validate('O', [props])
 
         // console.log(`props`, props)
 
