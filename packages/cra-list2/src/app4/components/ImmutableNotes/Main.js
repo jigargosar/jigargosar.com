49c7fba6264b92e2e9a568d6426c4ef873d1fe9d
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-15 16:32:01 +0530

    cra-list2: Immutable Notes Tree

--- packages/cra-list2/src/app4/components/ImmutableNotes/Main.js
+++ packages/cra-list2/src/app4/components/ImmutableNotes/Main.js
@@ -2,28 +2,49 @@ import React from 'react'
 import {CenterLayout, Title, TypographyDefaults} from '../ui'
 import {cn, mrInjectAll} from '../utils'
 import {AppHeaderBar} from '../mobx/AppHeaderBar'
-import {_} from '../../little-ramda'
+import {_, mapIndexed} from '../../little-ramda'
 
 function createNote({text = ''}) {
   return {text, children: []}
 }
 
-function addChild(child) {
+function appendChild(child) {
   return function(note) {
     return _.assoc('children')(_.append(child, note.children))(note)
   }
 }
 
+function getChildren({children}) {
+  return children
+}
+
 class NoteTree extends React.Component {
   state = {
-    root: addChild(createNote({text: 'first child'}))(
-      createNote({text: 'Tree Root'}),
-    ),
+    root: _.compose(
+      appendChild(createNote({text: 'second child'})),
+      appendChild(createNote({text: 'first child'})),
+    )(createNote({text: 'Tree Root'})),
+  }
+  renderChild = (child, idx) => {
+    return <div key={idx}>{child.text}</div>
+  }
+
+  renderChildren = note => {
+    return (
+      <div className={cn('ml3')}>
+        {mapIndexed(this.renderChild)(getChildren(note))}
+      </div>
+    )
   }
 
   render() {
     const {root} = this.state
-    return <div>{root.text}</div>
+    return (
+      <div className={cn('ma3')}>
+        <div>{root.text}</div>
+        {this.renderChildren(root)}
+      </div>
+    )
   }
 }
 
