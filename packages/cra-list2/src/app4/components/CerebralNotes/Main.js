a4f413bd042c8f5e475905f4e345ff4589817186
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-17 10:06:10 +0530

    cra-list2: Cerebral Notes Tree

--- packages/cra-list2/src/app4/components/CerebralNotes/Main.js
+++ packages/cra-list2/src/app4/components/CerebralNotes/Main.js
@@ -1,25 +1,16 @@
 import React from 'react'
 import {CenterLayout, Title, TypographyDefaults} from '../ui'
-import {
-  cn,
-  F,
-  setFocusAndSelectionOnDOMId,
-  whenKey,
-  withKeyEvent,
-} from '../utils'
+import {cn, F, whenKey, withKeyEvent} from '../utils'
 import {AppHeaderBar} from '../mobx/AppHeaderBar'
 import {
   connect,
   Container,
-  Controller,
-  Module,
   props,
   signal,
   state,
 } from '../../little-cerebral'
-import {_, isNotNil, validate} from '../../little-ramda'
-import {nanoid} from '../../model/util'
-import {StorageItem} from '../../services/storage'
+import {_, validate} from '../../little-ramda'
+import {controller} from '../../CerebralNotesState/controller'
 
 const NoteTextInput = connect(
   {
@@ -131,152 +122,6 @@ const NoteTree = connect({id: state`rootNoteId`}, function NoteTree({
     </F>
   )
 })
-function createNewNote({text, parentId = null}) {
-  return {id: nanoid(), text: text, parentId}
-}
-
-function createAppController() {
-  function getDevTools() {
-    if (module.hot) {
-      return require('cerebral/devtools').default({
-        host: 'localhost:8585',
-        reconnect: true,
-      })
-    }
-    return null
-  }
-
-  const storedState = StorageItem({
-    name: 'CerebralNoteTreeState',
-    getInitial: () => {
-      const rootNote = createNewNote({text: 'Root Note Title'})
-      const rootNoteId = rootNote.id
-
-      const initialState = {
-        rootNoteId,
-        childrenLookup: {[rootNoteId]: []},
-        noteLookup: {[rootNoteId]: rootNote},
-        currentRootNoteId: rootNoteId,
-      }
-      return initialState
-    },
-    postLoad: state => {
-      const ns = _.merge(state, {
-        childrenLookup: _.compose(
-          _.merge(_.map(() => [])(state.noteLookup)),
-          _.map(_.map(_.prop('id'))),
-          _.groupBy(_.prop('parentId')),
-          _.values,
-        )(state.noteLookup),
-      })
-      console.log(`ns`, ns)
-
-      return ns
-    },
-  })
-
-  const initialState = storedState.load()
-  setFocusAndSelectionOnDOMId(initialState.rootNoteId)
-
-  function getNote(id, state) {
-    return state.get(`noteLookup.${id}`)
-  }
-
-  function hasParent(id, state) {
-    const note = getNote(id, state)
-    return isNotNil(note.parentId)
-  }
-
-  function getParentId(id, state) {
-    const note = getNote(id, state)
-    return note.parentId
-  }
-
-  function getParent(id, state) {
-    return getNote(getParentId(id, state), state)
-  }
-
-  function getChildren(id, state) {
-    return state.get(`childrenLookup.${id}`)
-  }
-  function getSiblings(id, state) {
-    const parent = getParent(id, state)
-    return getChildren(parent.id, state)
-  }
-  function getIndexOf(id, state) {
-    const siblingNotes = getSiblings(id, state)
-    return _.indexOf(id, siblingNotes)
-  }
-
-  const app = Module({
-    // Define module state, namespaced by module path
-    state: {...initialState},
-    signals: {
-      setText: ({state, props}) => {
-        state.set(`noteLookup.${props.id}.text`, props.text)
-      },
-      prependNewChild: ({state, props}) => {
-        const parentId = props.id
-        const childNote = createNewNote({
-          text: nanoid(7),
-          parentId: parentId,
-        })
-        const childId = childNote.id
-        state.unshift(`childrenLookup.${parentId}`, childId)
-        state.set(`childrenLookup.${childId}`, [])
-        state.set(`noteLookup.${childId}`, childNote)
-
-        setFocusAndSelectionOnDOMId(childId)
-      },
-      appendSibling: ({state, props}) => {
-        if (!hasParent(props.id, state)) {
-          return
-        }
-
-        const idx = getIndexOf(props.id, state)
-        const newNote = createNewNote({
-          text: nanoid(7),
-          parentId: getParentId(props.id, state),
-        })
-        const childId = newNote.id
-
-        const childrenIds = state.get(
-          `childrenLookup.${newNote.parentId}`,
-        )
-
-        state.set(
-          `childrenLookup.${newNote.parentId}`,
-          _.insert(idx + 1)(childId)(childrenIds),
-        )
-
-        state.set(`childrenLookup.${childId}`, [])
-        state.set(`noteLookup.${childId}`, newNote)
-
-        setFocusAndSelectionOnDOMId(childId)
-      },
-    },
-    modules: {},
-    providers: {},
-    catch: [],
-  })
-
-  const controller = Controller(app, {
-    devtools: getDevTools(),
-  })
-
-  // controller.on('mutation', mutation => {
-  //   console.log(`mutation`, mutation)
-  // })
-
-  controller.on('flush', changes => {
-    console.debug(`changes`, changes)
-    storedState.save(controller.getState())
-  })
-
-  return controller
-}
-
-const controller = createAppController()
 
 function Main() {
   return (
