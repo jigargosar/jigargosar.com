79b9e195824523f1071157298833bcf8cb60e64b
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-17 14:37:35 +0530

    cra-list2: Cerebral Listy

--- packages/cra-list2/src/app4/components/CerebralListy/Main.js
+++ packages/cra-list2/src/app4/components/CerebralListy/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {CenterLayout, Title, TypographyDefaults} from '../ui'
-import {cn, F, whenKey, withKeyEvent} from '../utils'
+import {cn, F, renderKeyedById, whenKey, withKeyEvent} from '../utils'
 import {AppHeaderBar} from '../mobx/AppHeaderBar'
 import {
   connect,
@@ -12,116 +12,115 @@ import {
 import {_, validate} from '../../little-ramda'
 import {controller} from '../../CerebralListyState/controller'
 
-const NoteTextInput = connect(
-  {
-    setText: signal`setText`,
-    prependNewChild: signal`prependNewChild`,
-    appendSibling: signal`appendSibling`,
-    value: state`noteLookup.${props`id`}.text`,
-  },
-  function({setText, value, prependNewChild, appendSibling}, {id}) {
-    validate('FS', [setText, value])
+// const NoteTextInput = connect(
+//   {
+//     setText: signal`setText`,
+//     prependNewChild: signal`prependNewChild`,
+//     appendSibling: signal`appendSibling`,
+//     value: state`noteLookup.${props`id`}.text`,
+//   },
+//   function({setText, value, prependNewChild, appendSibling}, {id}) {
+//     validate('FS', [setText, value])
+//
+//     return {
+//       id,
+//       onChange: e =>
+//         setText({
+//           text: e.target.value,
+//           id,
+//         }),
+//       value,
+//       onKeyDown: withKeyEvent(
+//         whenKey('enter')(() => appendSibling({id})),
+//         whenKey('alt+enter')(() => prependNewChild({id})),
+//         // whenKey('backspace')(onBackspaceKeyDown),
+//         // whenKey('tab')(wrapPD(indentNote)),
+//         // whenKey('shift+tab')(wrapPD(unIndentNote)),
+//         // whenKey('down')(wrapPD(navigateToNextNote)),
+//         // whenKey('up')(wrapPD(navigateToPreviousNote)),
+//         // whenKey('shift+up')(wrapPD(() => collapseNote(note))),
+//         // whenKey('shift+down')(wrapPD(() => expandNote(note))),
+//         // whenKey('mod+.')(wrapPD(() => setCurrentRootNote(note))),
+//         // whenKey('mod+,')(wrapPD(setCurrentRootNoteOneLevelUp)),
+//       ),
+//     }
+//   },
+//   function NoteTextInput({id, value, onChange, onKeyDown}) {
+//     return (
+//       <input
+//         id={id}
+//         className={cn('flex-auto', 'ma0 pv2 bw0 outline-0')}
+//         value={value}
+//         onChange={onChange}
+//         onKeyDown={onKeyDown}
+//       />
+//     )
+//   },
+// )
+//
+// function NoteTextLine({id}) {
+//   return (
+//     <div className={cn('code flex items-center')}>
+//       <div className={cn('mr3')}>
+//         {/*{isNoteExpanded(note) ? `-` : `+`}*/}
+//         {`-`}
+//       </div>
+//       <div
+//         className={cn(
+//           'flex-auto',
+//           'flex items-center',
+//           'bb bw1 b--light-gray',
+//         )}
+//       >
+//         <div className={cn('f6 gray mr3', 'dn_')}>
+//           {id.slice(0, 3)}
+//         </div>
+//         <div className={cn('flex-auto', 'flex')}>
+//           <NoteTextInput id={id} />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+//
+// const NoteChildren = connect(
+//   {childrenIds: state`childrenLookup.${props`id`}`},
+//   function NoteChildren({childrenIds}) {
+//     // if (doesNoteHaveVisibleChildren(note)) {
+//     // debugger
+//     return (
+//       <div className={cn('ml3')}>
+//         {_.map(id => (
+//           <F key={id}>
+//             <NoteChild id={id} />
+//           </F>
+//         ))(childrenIds)}
+//       </div>
+//     )
+//     // } else {
+//     //   return null
+//     // }
+//   },
+// )
+// function NoteChild({id}) {
+//   return (
+//     <F>
+//       <NoteTextLine id={id} />
+//       <NoteChildren id={id} />
+//     </F>
+//   )
+// }
 
-    return {
-      id,
-      onChange: e =>
-        setText({
-          text: e.target.value,
-          id,
-        }),
-      value,
-      onKeyDown: withKeyEvent(
-        whenKey('enter')(() => appendSibling({id})),
-        whenKey('alt+enter')(() => prependNewChild({id})),
-        // whenKey('backspace')(onBackspaceKeyDown),
-        // whenKey('tab')(wrapPD(indentNote)),
-        // whenKey('shift+tab')(wrapPD(unIndentNote)),
-        // whenKey('down')(wrapPD(navigateToNextNote)),
-        // whenKey('up')(wrapPD(navigateToPreviousNote)),
-        // whenKey('shift+up')(wrapPD(() => collapseNote(note))),
-        // whenKey('shift+down')(wrapPD(() => expandNote(note))),
-        // whenKey('mod+.')(wrapPD(() => setCurrentRootNote(note))),
-        // whenKey('mod+,')(wrapPD(setCurrentRootNoteOneLevelUp)),
-      ),
-    }
-  },
-  function NoteTextInput({id, value, onChange, onKeyDown}) {
+const ListDashboard = connect(
+  {dashboard: state`currentDashboard`},
+  function ListDashboard({dashboard}) {
     return (
-      <input
-        id={id}
-        className={cn('flex-auto', 'ma0 pv2 bw0 outline-0')}
-        value={value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-      />
+      <F>
+        {renderKeyedById(TodoList, 'todoList', dashboard.todoList)}
+      </F>
     )
   },
 )
-
-function NoteTextLine({id}) {
-  return (
-    <div className={cn('code flex items-center')}>
-      <div className={cn('mr3')}>
-        {/*{isNoteExpanded(note) ? `-` : `+`}*/}
-        {`-`}
-      </div>
-      <div
-        className={cn(
-          'flex-auto',
-          'flex items-center',
-          'bb bw1 b--light-gray',
-        )}
-      >
-        <div className={cn('f6 gray mr3', 'dn_')}>
-          {id.slice(0, 3)}
-        </div>
-        <div className={cn('flex-auto', 'flex')}>
-          <NoteTextInput id={id} />
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const NoteChildren = connect(
-  {childrenIds: state`childrenLookup.${props`id`}`},
-  function NoteChildren({childrenIds}) {
-    // if (doesNoteHaveVisibleChildren(note)) {
-    // debugger
-    return (
-      <div className={cn('ml3')}>
-        {_.map(id => (
-          <F key={id}>
-            <NoteChild id={id} />
-          </F>
-        ))(childrenIds)}
-      </div>
-    )
-    // } else {
-    //   return null
-    // }
-  },
-)
-function NoteChild({id}) {
-  return (
-    <F>
-      <NoteTextLine id={id} />
-      <NoteChildren id={id} />
-    </F>
-  )
-}
-
-const NoteTree = connect({id: state`rootNoteId`}, function NoteTree({
-  id,
-}) {
-  return (
-    <F>
-      <div className={cn('ma3 pa3 shadow-1 bg-white')}>
-        <NoteChild id={id} />
-      </div>
-    </F>
-  )
-})
 
 function ListyMain() {
   return (
@@ -133,7 +132,7 @@ function ListyMain() {
           </Title>
         </AppHeaderBar>
         <CenterLayout>
-          <NoteTree />
+          <ListDashboard />
         </CenterLayout>
       </TypographyDefaults>
     </Container>
