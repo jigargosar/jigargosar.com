b5305fc56e2c30904688a3c227db27cb518a7737
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-19 21:49:30 +0530

    cra-list2: Cerebral Listy

---
+++ packages/cra-list2/src/app4/components/CerebralListy/Header.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-func-assign*/
+import React from 'react'
+import {CenterLayout, TypographyDefaults} from '../ui'
+import {cn, wrapPD} from '../utils'
+import {
+  connect,
+  Container,
+  signal,
+  state,
+} from '../../little-cerebral'
+import {_, idEq} from '../../little-ramda'
+import {controller} from '../../CerebralListyState/controller'
+import {Add} from '@material-ui/icons'
+
+function headerLinkCN({isSelected = false}) {
+  return cn(
+    'input-reset button-reset',
+    'link hover-z-1',
+    'bn',
+    'pa2',
+    'code pointer',
+    isSelected
+      ? 'black bg-white-80 o-hover-black-70'
+      : 'black hover-bg-white-20',
+  )
+}
+
+export const DashboardHeaderTabs = connect(
+  {
+    dashboards: state`dashboards`,
+    currentDashboardId: state`currentDashboardId`,
+    switchDashboard: signal`switchDashboard`,
+  },
+  function DashboardHeaderTabs({
+    dashboards,
+    currentDashboardId,
+    switchDashboard,
+  }) {
+    return _.map(dashboard => {
+      const isSelected = idEq(currentDashboardId, dashboard)
+      return (
+        <a
+          onClick={wrapPD(() => switchDashboard({dashboard}))}
+          href={`/dashboard/${dashboard.id}/${dashboard.name}`}
+          key={dashboard.id}
+          tabIndex={0}
+          className={headerLinkCN({isSelected})}
+        >
+          {dashboard.name}
+        </a>
+      )
+    })(dashboards)
+  },
+)
+
+export function Header({children}) {
+  return (
+    <div className={cn('black bg-light-blue', 'bb b--moon-gray')}>
+      <CenterLayout className={cn('flex items-center', 'pv1 pv2-ns')}>
+        <div className={cn('', 'flex ml3')}>{children}</div>
+        <div className={cn('flex items-center mh3')}>
+          <Add />
+        </div>
+        <div className={cn('flex-auto', 'flex mh3')} />
+        <div className={cn('flex f5 fw3 lh-title mh3')}>
+          <a className={cn('link ml2 pointer')}>Help</a>
+          <a className={cn('link ml2 pointer')}>Settings</a>
+        </div>
+      </CenterLayout>
+    </div>
+  )
+}
--- packages/cra-list2/src/app4/components/CerebralListy/Main.js
+++ packages/cra-list2/src/app4/components/CerebralListy/Main.js
@@ -17,6 +17,7 @@ import {Add} from '@material-ui/icons'
 import {Btn} from '../ui/tui'
 import {ListPane} from './ListPane'
 import {Bucket} from './Bucket'
+import {DashboardHeaderTabs, Header} from './Header'
 
 const Dashboard = connect(
   {
@@ -40,65 +41,6 @@ const Dashboard = connect(
     )
   },
 )
-
-function headerLinkCN({isSelected = false}) {
-  return cn(
-    'input-reset button-reset',
-    'link hover-z-1',
-    'bn',
-    'pa2',
-    'code pointer',
-    isSelected
-      ? 'black bg-white-80 o-hover-black-70'
-      : 'black hover-bg-white-20',
-  )
-}
-
-const DashboardHeaderTabs = connect(
-  {
-    dashboards: state`dashboards`,
-    currentDashboardId: state`currentDashboardId`,
-    switchDashboard: signal`switchDashboard`,
-  },
-  function DashboardHeaderTabs({
-    dashboards,
-    currentDashboardId,
-    switchDashboard,
-  }) {
-    return _.map(dashboard => {
-      const isSelected = idEq(currentDashboardId, dashboard)
-      return (
-        <a
-          onClick={wrapPD(() => switchDashboard({dashboard}))}
-          href={`/dashboard/${dashboard.id}/${dashboard.name}`}
-          key={dashboard.id}
-          tabIndex={0}
-          className={headerLinkCN({isSelected})}
-        >
-          {dashboard.name}
-        </a>
-      )
-    })(dashboards)
-  },
-)
-
-function Header({children}) {
-  return (
-    <div className={cn('black bg-light-blue', 'bb b--moon-gray')}>
-      <CenterLayout className={cn('flex items-center', 'pv1 pv2-ns')}>
-        <div className={cn('', 'flex ml3')}>{children}</div>
-        <div className={cn('flex items-center mh3')}>
-          <Add />
-        </div>
-        <div className={cn('flex-auto', 'flex mh3')} />
-        <div className={cn('flex f5 fw3 lh-title mh3')}>
-          <a className={cn('link ml2 pointer')}>Help</a>
-          <a className={cn('link ml2 pointer')}>Settings</a>
-        </div>
-      </CenterLayout>
-    </div>
-  )
-}
 
 const CurrentDashboard = connect(
   {currentDashboardId: state`currentDashboardId`},
