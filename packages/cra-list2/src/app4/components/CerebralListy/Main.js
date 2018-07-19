59836d0615a08f7ef9b5d7b64e79bcaf9eb5efc4
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-19 21:50:38 +0530

    cra-list2: Cerebral Listy

--- packages/cra-list2/src/app4/components/CerebralListy/Main.js
+++ packages/cra-list2/src/app4/components/CerebralListy/Main.js
@@ -2,20 +2,8 @@
 import React from 'react'
 import {TypographyDefaults} from '../ui'
 import {cn} from '../utils'
-import {
-  connect,
-  Container,
-  signal,
-  state,
-} from '../../little-cerebral'
-import {_} from '../../little-ramda'
-import {
-  controller,
-  dashboardIdToBucketIds,
-} from '../../CerebralListyState/controller'
-import {Btn} from '../ui/tui'
-import {ListPane} from './ListPane'
-import {Bucket} from './Bucket'
+import {Container} from '../../little-cerebral'
+import {controller} from '../../CerebralListyState/controller'
 import {DashboardHeaderTabs, Header} from './Header'
 import {CurrentDashboard} from './Dashboard'
 
