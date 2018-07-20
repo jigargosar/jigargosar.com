269c175e3da0e24571a709e2b569fd3426070955
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-20 10:13:41 +0530

    cra-list2: Cerebral Listy

--- packages/cra-list2/src/app4/components/MSTListy/Main.js
+++ packages/cra-list2/src/app4/components/MSTListy/Main.js
@@ -2,22 +2,14 @@
 import React from 'react'
 import {TypographyDefaults} from '../ui'
 import {cn} from '../utils'
-import {Container} from '../../little-cerebral'
-import {controller} from '../../cerebral/CerebralListyState/controller'
-import {DashboardHeaderTabs, Header} from './Header'
 import {Dashboard} from './Dashboard'
 import {observer} from 'mobx-react'
 
 function ListyMain() {
   return (
-    <Container controller={controller}>
-      <TypographyDefaults className={cn('mb4')}>
-        <Header>
-          <DashboardHeaderTabs />
-        </Header>
-        <Dashboard />
-      </TypographyDefaults>
-    </Container>
+    <TypographyDefaults className={cn('mb4')}>
+      <Dashboard />
+    </TypographyDefaults>
   )
 }
 
