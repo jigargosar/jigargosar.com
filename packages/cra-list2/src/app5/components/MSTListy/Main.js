9c54a051714f8b79b7223e4c39954bfe5c3a20ed
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-21 20:34:59 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/components/MSTListy/Main.js
+++ packages/cra-list2/src/app5/components/MSTListy/Main.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-func-assign*/
-import React from 'react'
+import PropTypes from 'prop-types'
+import React, {Fragment} from 'react'
 import {StyleRoot} from '../styled'
 import {whenKey, withKeyEvent} from '../utils'
 import {Dashboard} from './Dashboard'
 import {oInject} from './utils'
 import {_} from '../../little-ramda'
+import {
+  Inspector,
+  ObjectName,
+  ObjectRootLabel,
+  ObjectValue,
+} from 'react-inspector'
+import {Domain} from '../../mst/listy-stores/collection-stores'
+// import {Domain} from '../../mst/listy-stores/collection-stores'
 
 class KeyboardShortcuts extends React.Component {
   componentDidMount() {
@@ -28,11 +37,90 @@ class KeyboardShortcuts extends React.Component {
   }
 }
 
+const ObjectLabel = ({name, data, isNonenumerable}) => {
+  const object = data
+
+  return (
+    <span>
+      <ObjectName name={name} dimmed={isNonenumerable} />
+      <span>: </span>
+      {typeof object === 'function' ? (
+        object.toString()
+      ) : (
+        <ObjectValue object={object} />
+      )}
+    </span>
+  )
+}
+
+ObjectLabel.propTypes = {
+  data: PropTypes.any,
+  isNonenumerable: PropTypes.bool,
+  name: PropTypes.any,
+}
+
+ObjectLabel.defaultProps = {
+  isNonenumerable: false,
+}
+
+const defaultNodeRenderer = ({
+  depth,
+  name,
+  data,
+  isNonenumerable,
+}) => {
+  console.log(`data`, data)
+
+  return depth === 0 ? (
+    <ObjectRootLabel name={name} data={data} />
+  ) : (
+    <ObjectLabel
+      name={name}
+      data={data}
+      isNonenumerable={isNonenumerable}
+    />
+  )
+}
+
+function INS() {
+  return (
+    <Fragment>
+      <Inspector
+        name={'Domain'}
+        data={Domain}
+        showNonenumerable
+        expandLevel={2}
+        nodeRenderer={defaultNodeRenderer}
+      />
+      <Inspector
+        expandLevel={1}
+        data={{
+          a1: 1,
+          a2: 'A2',
+          a3: true,
+          a4: undefined,
+          a5: {
+            'a5-1': null,
+            'a5-2': ['a5-2-1', 'a5-2-2'],
+            'a5-3': {},
+          },
+          a6: function() {
+            console.log('hello world')
+          },
+          a7: new Date('2005-04-03'),
+        }}
+        nodeRenderer={defaultNodeRenderer}
+      />
+    </Fragment>
+  )
+}
+
 function ListyMain({store}) {
   return (
     <StyleRoot>
       <KeyboardShortcuts store={store} />
       <Dashboard dashboard={store} />
+      <INS />
     </StyleRoot>
   )
 }
