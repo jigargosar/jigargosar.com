e09ba479b255df6752719bcb9332ca02a7f3eef4
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-21 16:47:38 +0530

    cra-list2: Cerebral Listy: delete unused

--- packages/cra-list2/src/app5/mst/Model.js
+++ packages/cra-list2/src/app5/mst/Model.js
@@ -1,5 +1,15 @@
 import {types} from 'mobx-state-tree'
+import {modelId, nanoid} from '../little-model'
+import {R} from '../little-ramda'
 
 export function Model({name, attrs}) {
-  return types.model(name, {attrs: types.model('Attr', attrs)})
+  return types.model(name, {
+    attrs: types.model(
+      'Attr',
+      R.merge(attrs, {
+        id: types.optional(types.identifier, () => modelId(name)),
+        name: '',
+      }),
+    ),
+  })
 }
