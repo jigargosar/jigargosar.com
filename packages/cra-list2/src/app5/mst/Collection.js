41086ce82d8178dccc0b6b291b4497bfd80a70a5
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-21 17:07:33 +0530

    cra-list2: Cerebral Listy: delete unused

--- packages/cra-list2/src/app5/mst/Collection.js
+++ packages/cra-list2/src/app5/mst/Collection.js
@@ -1,16 +1,30 @@
 import {types} from 'mobx-state-tree'
+import {R} from '../little-ramda'
+import {mValues} from '../mobx/little-mobx'
 
 export function Collection({model}) {
   return types
     .model(`${model.name}Collection`, {
-      lookup: model,
+      lookup: types.map(model),
     })
     .views(ext)
 
   function ext(self) {
     return {
+      get list() {
+        return mValues(self.lookup)
+      },
       get(id) {
         return self.lookup.get(id)
+      },
+      whereEq(attrs) {
+        return R.whereEq(attrs)(self.list)
+      },
+      filter(pred) {
+        return R.filter(pred)(self.list)
+      },
+      reject(pred) {
+        return R.reject(pred)(self.list)
       },
     }
   }
