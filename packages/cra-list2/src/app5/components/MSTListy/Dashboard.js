c954be7e8e2d6e9477516e15a8404bcb7eb54751
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-22 18:49:45 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/components/MSTListy/Dashboard.js
+++ packages/cra-list2/src/app5/components/MSTListy/Dashboard.js
@@ -15,10 +15,6 @@ const Layout = system({
 const Panel = system({
   is: B.Box,
   width: [1, 0.8, 1 / 2, 1 / 3, 1 / 4],
-
-  // minWidth: '350px',
-  // maxWidth: '34em',
-  // flex: '1 1 auto',
   p: 3,
   mx: [0, 'auto', 0],
   border: 1,
