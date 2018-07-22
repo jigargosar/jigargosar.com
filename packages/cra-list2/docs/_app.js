25b0e9982b71622fa918fe5abca9b06ce5461e70
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-22 13:29:49 +0530

    cra-list2: MST Listy

--- packages/cra-list2/docs/_app.js
+++ packages/cra-list2/docs/_app.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 import {Link} from 'react-router-dom'
 import {renderKeyedByProp} from '../src/app5/components/utils'
 import {Inspector} from '../src/little-exports'
+import {SidebarLayout} from '@compositor/x0/components'
 
 function NavLink({route: {path, name}}) {
   return (
@@ -26,6 +27,9 @@ export default class extends React.Component {
 
     return (
       <Fragment>
+        <SidebarLayout {...this.props} title={'Listy X0X0'}>
+          {render()}
+        </SidebarLayout>
         {renderKeyedByProp(NavLink, 'route', 'path', routes)}
         <Inspector data={this.props} expandLevel={1} />
         {/*<Inspector data={this.props.render()} expandLevel={1} />*/}
