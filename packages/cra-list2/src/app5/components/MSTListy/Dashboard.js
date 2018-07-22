8e8d829a7a1237ba0bfac0feec58d4f3a7b47d46
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-22 15:56:31 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/components/MSTListy/Dashboard.js
+++ packages/cra-list2/src/app5/components/MSTListy/Dashboard.js
@@ -1,25 +1,24 @@
 /* eslint-disable no-func-assign*/
 import React from 'react'
-import {cn, renderKeyedById} from '../utils'
-import {Btn} from '../tui'
-import {ListPane} from './ListPane'
+import {renderKeyedById} from '../utils'
 import {Bucket} from './Bucket'
 import {observer} from 'mobx-react'
 import {REB} from '../REB'
 
 function Dashboard({dashboard}) {
   return (
-    <REB.Flex className={cn('flex flex-wrap')}>
+    <REB.Flex flexWrap={'wrap'}>
       {renderKeyedById(Bucket, 'bucket', dashboard.buckets)}
-      <ListPane>
-        <ListPane.Item
-          Component={Btn}
-          colors={'black-50 hover-black-80 hover-bg-black-10'}
+      <REB.Flex
+        flex={'1 1 auto'}
+        flexDirection={'column'}
+        // align={'stretch'}
+      >
+        <REB.Button
           onClick={() => dashboard.addBucket()}
-        >
-          <ListPane.ItemText>{`Add List`}</ListPane.ItemText>
-        </ListPane.Item>
-      </ListPane>
+          children={'Add List'}
+        />
+      </REB.Flex>
     </REB.Flex>
   )
 }
