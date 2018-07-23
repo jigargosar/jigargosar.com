7f818afad6f4e5c1a988d19208fe8a65a5df006e
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-23 17:41:17 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/components/MSTListy/Bucket.js
+++ packages/cra-list2/src/app5/components/MSTListy/Bucket.js
@@ -20,10 +20,21 @@ export const BucketItemBtn = styled(Btn).attrs({
   width: 1,
 })``
 
+export const BucketHeaderLayout = styled(FlexRow).attrs({
+  mb: 2,
+  pl: 1,
+  colors: 'dim',
+  variant: 'default',
+  tabIndex: 0,
+})`
+  ${buttonStyle};
+`
+
 export const BucketItemLayout = styled(FlexRow).attrs({
   pl: modularScale(0.5),
   width: 1,
   variant: 'default',
+  tabIndex: 0,
 })`
   ${buttonStyle};
 `
@@ -34,7 +45,6 @@ const BucketItem = observer(function BucketItem({item}) {
       id={item.id}
       onFocus={item.onFocus}
       onBlur={item.onBlur}
-      tabIndex={0}
       onKeyDown={withKeyEvent(
         whenKeyPD('up')(item.onNavigatePrev),
         whenKeyPD('down')(item.onNavigateNext),
@@ -63,12 +73,12 @@ const BucketItems = observer(function BucketItems({bucket}) {
 function Bucket({bucket}) {
   return (
     <Fragment>
-      <FlexRow pb={2} colors={'dim'}>
+      <BucketHeaderLayout>
         <Box textStyle={'bucketTitle'} flex={1}>
           {bucket.name || 'I am a Bucket Short and Stout'}
         </Box>
         <IconBtn icon={Delete} />
-      </FlexRow>
+      </BucketHeaderLayout>
       <BucketItems bucket={bucket} />
       <BucketItemBtn
         children={'Add Item'}
