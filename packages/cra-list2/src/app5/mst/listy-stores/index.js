322dfccc7b2b52f90352e1d50343267b62ff9d8f
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-23 16:31:37 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/mst/listy-stores/index.js
+++ packages/cra-list2/src/app5/mst/listy-stores/index.js
@@ -1,10 +1,22 @@
 import rootStore from './root-store'
 import {Root} from './collection-stores'
+import {dotPath, isNotNil, R} from '../../little-ramda'
+import {applySnapshot2} from '../../little-mst'
+import {getSnapshot} from 'mobx-state-tree'
 
 const store = rootStore
 
 const root = Root.create()
 const domain = root.domain
 
-domain.addMockData()
 export {domain, store}
+
+if (module.hot) {
+  console.debug(`store`, store)
+  domain.addMockData()
+
+  const snap = dotPath('hot.data.snap')(module)
+  R.ifElse(isNotNil)(applySnapshot2(root))(domain.addMockData)(snap)
+
+  module.hot.dispose(data => (data.snap = getSnapshot(root)))
+}
