9e820911766b03481a9babcb760e11054a13fdc5
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-25 21:37:46 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/mst/Collection.js
+++ packages/cra-list2/src/app5/mst/Collection.js
@@ -1,7 +1,16 @@
 import {types} from 'mobx-state-tree'
 import {mValues} from '../mobx/little-mobx'
 import {modelNamed} from '../little-mst'
-import * as R from 'ramda'
+import {
+  T,
+  whereEq,
+  filter,
+  reject,
+  find,
+  compose,
+  map,
+  forEach,
+} from 'ramda'
 
 export function Collection(Model) {
   return modelNamed(`${Model.name}Collection`)
@@ -13,15 +22,15 @@ export function Collection(Model) {
         return mValues(self.lookup)
       },
       get: id => self.lookup.get(id),
-      whereEq: attrs => self.filter(R.whereEq(attrs)),
-      filter: pred => R.filter(pred)(self.list),
-      reject: pred => R.reject(pred)(self.list),
-      find: pred => R.find(pred || R.T)(self.list),
+      whereEq: attrs => self.filter(whereEq(attrs)),
+      filter: pred => filter(pred)(self.list),
+      reject: pred => reject(pred)(self.list),
+      find: pred => find(pred || T)(self.list),
     }))
     .actions(self => ({
       add: m => self.lookup.put(Model.create(m)),
-      addAll: R.map(self.add),
+      addAll: map(self.add),
       delete: m => self.lookup.delete(m.id),
-      deleteAll: R.forEach(self.delete),
+      deleteAll: forEach(self.delete),
     }))
 }
