dbe4991f6fae5e2843b1c65ef2ab6d6ff1c1b768
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-25 21:36:17 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/mst/Collection.js
+++ packages/cra-list2/src/app5/mst/Collection.js
@@ -3,14 +3,7 @@ import {mValues} from '../mobx/little-mobx'
 import {modelNamed} from '../little-mst'
 import * as R from 'ramda'
 
-const deleteMapKey = R.curry((key, map) => map.delete(key))
-const putMapObj = R.curry((obj, map) => map.put(obj))
-
 export function Collection(Model) {
-  function createModel(snap) {
-    return Model.create(snap)
-  }
-
   return modelNamed(`${Model.name}Collection`)
     .props({
       lookup: types.map(Model),
@@ -19,27 +12,16 @@ export function Collection(Model) {
       get list() {
         return mValues(self.lookup)
       },
-      get(id) {
-        return self.lookup.get(id)
-      },
-      whereEq(attrs) {
-        return self.filter(R.whereEq(attrs))
-      },
-      filter(pred) {
-        return R.filter(pred)(self.list)
-      },
-      reject(pred) {
-        return R.reject(pred)(self.list)
-      },
-      find(pred = R.T) {
-        return R.find(pred)(self.list)
-      },
+      get: id => self.lookup.get(id),
+      whereEq: attrs => self.filter(R.whereEq(attrs)),
+      filter: pred => R.filter(pred)(self.list),
+      reject: pred => R.reject(pred)(self.list),
+      find: pred => R.find(pred || R.T)(self.list),
     }))
     .actions(self => ({
-      add: R.compose(putMapObj(R.__, self.lookup), createModel),
+      add: m => self.lookup.put(Model.create(m)),
       addAll: R.map(self.add),
-      deleteId: deleteMapKey(R.__, self.lookup),
-      delete: R.compose(self.deleteId, R.prop('id')),
+      delete: m => self.lookup.delete(m.id),
       deleteAll: R.forEach(self.delete),
     }))
 }
