f64ec23b0731fc35dbb827ccb0f7725c05292847
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-27 08:46:14 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/mst/listy-stores/SelectionManager.js
+++ packages/cra-list2/src/app5/mst/listy-stores/SelectionManager.js
@@ -2,7 +2,7 @@ import {modelNamed} from '../../little-mst'
 import {types} from 'mobx-state-tree'
 import {setFocusAndSelectionOnDOMId} from '../../components/utils'
 import * as R from 'ramda'
-import {C, maybeOr_, maybeOrElse} from '../../little-ramda'
+import {maybeOr_, maybeOrElse} from '../../little-ramda'
 import S from 'sanctuary'
 import {Bucket, Dashboard, Item} from './models'
 import {getCurrentDashboard} from './helpers'
@@ -34,13 +34,41 @@ export const SelectionManager = modelNamed('SelectionManager')
   })
   .views(self => ({
     get selectedModel() {
-      return C(
-        maybeOrElse(() => getCurrentDashboard(self)),
-        S.toMaybe,
-      )(self._selectedModel)
+      return S.toMaybe(self._selectedModel)
+    },
+    get selectedModelOrCurrentDashboard() {
+      return maybeOrElse(() => getCurrentDashboard(self))(
+        self.selectedModel,
+      )
     },
   }))
   .actions(self => ({
+    clearSelection() {
+      self._selectedModel = null
+    },
+    deleteSelectionTree() {
+      const _selectedModel = self._selectedModel
+      if (_selectedModel) {
+        self.clearSelection()
+        _selectedModel.deleteTree()
+      }
+    },
+    setSelectionToModel(m) {
+      self._selectedModel = m
+      setFocusAndSelectionOnDOMId(m.id)
+    },
+    onDashboardMount(d) {
+      R.compose(
+        self.setSelectionToModel,
+        maybeOr_(() => R.compose(R.last, R.take(3))(d.flatNavModels)),
+      )(self.selectedModel)
+    },
+    onModelFocus(m) {
+      self._selectedModel = m
+    },
+    onModelBlur() {
+      self.clearSelection()
+    },
     tapSelectedModel(fn) {
       return S.map(fn)(self.selectedModel)
     },
@@ -64,31 +92,5 @@ export const SelectionManager = modelNamed('SelectionManager')
           (idx, models) => (idx === 0 ? models.length - 1 : idx - 1),
         ),
       )
-    },
-    setSelectionToModel(m) {
-      self._selectedModel = m
-      setFocusAndSelectionOnDOMId(m.id)
-    },
-    onDashboardMount(d) {
-      R.compose(
-        self.setSelectionToModel,
-        maybeOr_(() => R.compose(R.last, R.take(3))(d.flatNavModels)),
-      )(self.selectedModel)
-    },
-    onModelFocus(m) {
-      self._selectedModel = m
-    },
-    onModelBlur() {
-      self.clearSelection()
-    },
-    clearSelection() {
-      self._selectedModel = null
-    },
-    deleteSelectionTree() {
-      const _selectedModel = self._selectedModel
-      if (_selectedModel) {
-        self.clearSelection()
-        _selectedModel.deleteTree()
-      }
     },
   }))
