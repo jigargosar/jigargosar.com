e91cfa219128845af6c15fd0b9a012e4129bedce
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-28 10:47:06 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/mst/listy-stores/hasManyChildren.js
+++ packages/cra-list2/src/app5/mst/listy-stores/hasManyChildren.js
@@ -1,19 +1,23 @@
 import {getCollection, setSelectionToModel} from './helpers'
+import {R} from '../../little-ramda'
 
-export function hasManyChildren(childType) {
-  return self => ({
-    views: {
-      get childCollection() {
-        return getCollection(self, childType)
+export function hasManyChildren(typeOrFn) {
+  return self => {
+    const childType = R.when(R.is(Function))(fn => fn())(typeOrFn)
+    return {
+      views: {
+        get childCollection() {
+          return getCollection(self, childType)
+        },
+        get children() {
+          return self.childCollection.whereEq({parent: self})
+        },
       },
-      get children() {
-        return self.childCollection.whereEq({parent: self})
+      actions: {
+        onAddChild: () => setSelectionToModel(self.addChild({})),
+        addChild: attrs =>
+          self.childCollection.add({...attrs, parent: self}),
       },
-    },
-    actions: {
-      onAddChild: () => setSelectionToModel(self.addChild({})),
-      addChild: attrs =>
-        self.childCollection.add({...attrs, parent: self}),
-    },
-  })
+    }
+  }
 }
