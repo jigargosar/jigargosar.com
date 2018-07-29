85f6d33fe50b080f8efb31f7a6aac0adc74cba4b
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-29 16:57:39 +0530

    fun-react-notes

--- packages/fun-react-notes/src/index.js
+++ packages/fun-react-notes/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept(['./App'], () => {
+    ReactDOM.render(
+      require('./App').default,
+      document.getElementById('root'),
+    )
+  })
+}
