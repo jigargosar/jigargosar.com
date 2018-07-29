ecf218663cbf89d39c129d6a27fef2f488c4854d
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-29 14:52:14 +0530

    cra-list2: MST Listy

--- packages/cra-list2/src/app5/components/MSTListy/InputWrapper.js
+++ packages/cra-list2/src/app5/components/MSTListy/InputWrapper.js
@@ -5,7 +5,10 @@ import React from 'react'
 
 export const InputWrapper = function InputWrapper({children}) {
   return (
-    <FocusTrap className={cn('flex-auto flex')}>
+    <FocusTrap
+      className={cn('flex-auto flex')}
+      focusTrapOptions={{returnFocusOnDeactivate: false}}
+    >
       <AutoSize>{children}</AutoSize>
     </FocusTrap>
   )
