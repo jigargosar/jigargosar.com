3a23563e4f4953d68d246235763962a3626f84e9
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-29 16:55:55 +0530

    fun-react-notes

--- packages/fun-react-notes/src/App.js
+++ packages/fun-react-notes/src/App.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react'
 
 class App extends Component {
+  state = {
+    title: 'Welcome to React',
+    content: `To get started, edit <code>src/App.js</code> and save to reload.`,
+  }
+
   render() {
     return (
       <div>
         <header>
-          <h1>Welcome to React</h1>
+          <h1>{this.state.title}</h1>
         </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{this.state.content}</p>
       </div>
     )
   }
