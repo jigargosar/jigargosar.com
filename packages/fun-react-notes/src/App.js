0d006906dde6c0ed02fe3fe0d4e346372c5966ce
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-29 18:48:14 +0530

    fun-react-notes

--- packages/fun-react-notes/src/App.js
+++ packages/fun-react-notes/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
           {notes => (
             <Fragment>
               <Log notes={notes.value} />
+              <Log {...notes} />
             </Fragment>
           )}
         </ArrayValue>
