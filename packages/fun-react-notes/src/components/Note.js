fe3c68b8fb6457b07eff4ca8bb5ee740d233b819
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-30 14:53:18 +0530

    fun-react-notes

--- packages/fun-react-notes/src/components/Note.js
+++ packages/fun-react-notes/src/components/Note.js
@@ -5,13 +5,9 @@ import {whenKey, withKeyEvent} from './utils'
 import root from '../models'
 
 class Note extends Component {
-  render() {
-    const {note} = this.props
+  render({note}) {
     return (
       <div>
-        {/*<Log comp={'Note'} {...note} />*/}
-
-        {/*<div>{note.text}</div>*/}
         <AutoSize>
           <textarea
             style={{display: 'block', width: '100%'}}
@@ -27,17 +23,3 @@ class Note extends Component {
 }
 
 export default observer(Note)
-
-// class Log extends Component {
-//   render() {
-//     console.groupCollapsed('Props', this.props)
-//     _forEachObjIndexed((v, k) => {
-//       console.log('Log', k, v)
-//       if (_contains(_type(v))(['Object', 'Array'])) {
-//         console.table(v)
-//       }
-//     })(this.props)
-//     console.groupEnd('end')
-//     return null
-//   }
-// }
