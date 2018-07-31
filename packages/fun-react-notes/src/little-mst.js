6f7a9ce03b63857e702472ba1ae8045a1c9405f8
Jigar Gosar <jigar.gosar@gmail.com>
2018-07-31 10:18:19 +0530

    fun-react-notes

--- packages/fun-react-notes/src/little-mst.js
+++ packages/fun-react-notes/src/little-mst.js
@@ -1,12 +1,6 @@
 import {applySnapshot, getSnapshot, types} from 'mobx-state-tree'
 import nanoid from 'nanoid'
-import {
-  _compose,
-  _merge,
-  _path,
-  _startsWith,
-  forEachIndexed,
-} from './little-ramda'
+import {_compose, _merge, _path, _startsWith} from './little-ramda'
 
 export {
   addDisposer,
