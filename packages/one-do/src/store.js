a521bbbe20dd6a54806ed54ff33861e4b406b286
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-07 20:04:27 +0530

    one-do

--- packages/one-do/src/store.js
+++ packages/one-do/src/store.js
@@ -1,6 +1,18 @@
 import RootStore from './RootStore'
 import delay from 'delay'
 import {destroy} from './lib/little-mst'
+import firebase from 'firebase/app'
+
+const app =
+  firebase.apps[0] ||
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAve3E-llOy2_ly87mJMSvcWDG6Uqyq8PA',
+    authDomain: 'not-now-142808.firebaseapp.com',
+    databaseURL: 'https://not-now-142808.firebaseio.com',
+    projectId: 'not-now-142808',
+    storageBucket: 'not-now-142808.appspot.com',
+    messagingSenderId: '476064436883',
+  })
 
 const syncAdapter = {
   syncItem(name, props) {
