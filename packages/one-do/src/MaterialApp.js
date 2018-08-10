facc7c22c659eaaea26436121e5f910d3e4c9ed0
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-10 12:05:51 +0530

    one-do

--- packages/one-do/src/MaterialApp.js
+++ packages/one-do/src/MaterialApp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
@@ -8,6 +8,8 @@ import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import {mailFolderListItems, otherMailFolderListItems} from './tileData'
+import {observer} from './lib/little-react'
+import {observable} from './lib/little-mst'
 
 const drawerWidth = 240
 
@@ -27,6 +29,10 @@ const styles = theme => ({
     position: 'relative',
     width: drawerWidth,
   },
+  drawerPaperClosed: {
+    position: 'relative',
+    width: 0,
+  },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
@@ -36,37 +42,44 @@ const styles = theme => ({
   toolbar: theme.mixins.toolbar,
 })
 
-function ClippedDrawer(props) {
-  const {classes} = props
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="absolute" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="title" color="inherit" noWrap>
-            Clipped drawer
+@observer
+class ClippedDrawer extends Component {
+  @observable isDrawerOpen = false
+  render() {
+    const {classes} = this.props
+    const isDrawerOpen = this.isDrawerOpen
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              Clipped drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="persistent"
+          classes={{
+            paper: isDrawerOpen
+              ? classes.drawerPaper
+              : classes.drawerPaperClosed,
+          }}
+          open={isDrawerOpen}
+        >
+          <div className={classes.toolbar} />
+          <List>{mailFolderListItems}</List>
+          <Divider />
+          <List>{otherMailFolderListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography noWrap>
+            {'You think water moves fast? You should see ice.'}
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>{mailFolderListItems}</List>
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography noWrap>
-          {'You think water moves fast? You should see ice.'}
-        </Typography>
-      </main>
-    </div>
-  )
+        </main>
+      </div>
+    )
+  }
 }
 
 ClippedDrawer.propTypes = {
