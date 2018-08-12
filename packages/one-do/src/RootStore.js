fed804d9935dc2e391cde40e642024bb2c5c94db
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-12 13:14:35 +0530

    one-do

--- packages/one-do/src/RootStore.js
+++ packages/one-do/src/RootStore.js
@@ -19,9 +19,6 @@ import {
   ascend,
   compose,
   defaultTo,
-  equals,
-  filter,
-  isNil,
   pick,
   propEq,
   reject,
@@ -31,90 +28,13 @@ import {
 import {findById, overPath} from './lib/little-ramda'
 import {
   authStateKnownPromise,
-  firestoreUserCRefNamed,
   isSignedIn,
   isSignedOut,
-  queryToDocsData,
   signInWithPopup,
 } from './firebase'
 import {Layout} from './models/Layout'
 import {Selection} from './models/Selection'
-
-function collection(Model) {
-  const Collection = model(`${Model.name}Collection`, {
-    items: types.optional(types.array(Model), []),
-  })
-    .volatile(self => ({}))
-    .views(self => ({
-      get dirtyItems() {
-        return self.filter(_prop('isDirty'))
-      },
-      get activeItems() {
-        return reject(_prop('isDeleted'))(self.items)
-      },
-      get isDirty() {
-        return self.dirtyItems.length > 0
-      },
-      filter(fn) {
-        return filter(fn)(self.items)
-      },
-    }))
-    .actions(self => ({
-      add(props) {
-        self.items.push(Model.create(props))
-      },
-      update(props, item) {
-        const preUpdateSnap = item.remoteSnap
-        Object.assign(item, item.pickRemoteProps(props))
-        if (!item.isDirty && !equals(preUpdateSnap, item.remoteSnap)) {
-          item.isDirty = true
-        }
-      },
-      saveToCRef: flow(function*(cRef, item) {
-        console.assert(item.isDirty)
-        const preSaveFireSnap = item.remoteSnap
-        yield cRef.doc(item.id).set(preSaveFireSnap)
-        if (equals(preSaveFireSnap, item.remoteSnap)) {
-          item.isDirty = false
-        }
-      }),
-      loadFromRemoteData(data, item) {
-        if (item.isDirty) return
-        const cleanProps = _compose(reject(isNil), item.pickRemoteProps)(
-          data,
-        )
-        Object.assign(item, cleanProps)
-      },
-      delete(item) {
-        self.update({isDeleted: true}, item)
-      },
-      pullFromRemote: dropFlow(function*() {
-        console.assert(isSignedIn())
-        const cRef = firestoreUserCRefNamed(Collection.name)
-
-        const docsData = yield queryToDocsData(cRef)
-        console.log(`[sync] pull result: docsData.length`, docsData.length)
-        docsData.forEach(data => {
-          const item = findById(data.id)(self.items)
-          if (item) {
-            self.loadFromRemoteData(data, item)
-          } else {
-            self.add({...data, isDirty: false})
-          }
-        })
-      }),
-      pushDirtyToRemote: dropFlow(function*() {
-        console.assert(isSignedIn())
-        const cRef = firestoreUserCRefNamed(Collection.name)
-
-        const pushResult = yield Promise.all(
-          self.dirtyItems.map(item => self.saveToCRef(cRef, item)),
-        )
-        console.log('[sync] push success', pushResult.length)
-      }),
-    }))
-  return Collection
-}
+import {collection} from './models/Collection'
 
 const Task = model('Task', {
   id: modelId('Task'),
