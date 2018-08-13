5d1364d3f11ef9d1cdc1ebd1030195093ea21495
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-13 11:49:34 +0530

    one-do

--- packages/one-do/src/StoreActions.js
+++ packages/one-do/src/StoreActions.js
@@ -2,5 +2,6 @@ import store from './store'
 
 export const dispatch = actionName => (...args) => () =>
   store[actionName](...args)
+
 export const dispatchToggleDrawer = dispatch('toggleDrawer')
-export const dispatchAddTask = dispatch('toggleDrawer')
+export const dispatchAddTask = dispatch('addTask')
