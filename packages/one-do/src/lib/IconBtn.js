70d111cebb8982234b8cc999cf18f9ccad56477b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-13 18:44:19 +0530

    one-do

--- packages/one-do/src/lib/IconBtn.js
+++ packages/one-do/src/lib/IconBtn.js
@@ -7,14 +7,14 @@ export function IconBtn({label, Icon, ...other}) {
   return (
     <Btn {...other}>
       <div className={cn('flex flex-column items-center')}>
-        <Icon />
-        <div className={cn('f6')}>{label}</div>
+        {Icon && <Icon />}
+        {label && <div className={cn('f6')}>{label}</div>}
       </div>
     </Btn>
   )
 }
 
 IconBtn.propTypes = {
-  label: PropTypes.any,
+  label: PropTypes.string,
   Icon: PropTypes.any,
 }
