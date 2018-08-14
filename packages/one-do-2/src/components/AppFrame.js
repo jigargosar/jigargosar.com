95d0294c4cc1c8b52b762bb0ae0f89f9d68aabc8
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-14 20:39:28 +0530

    one-do-2

--- packages/one-do-2/src/components/AppFrame.js
+++ packages/one-do-2/src/components/AppFrame.js
@@ -2,17 +2,17 @@ import React from 'react'
 import {Component} from '../lib/little-mobx-react'
 import {observable} from '../lib/mobx'
 
+const store = observable({
+  counter: 0,
+})
+
+setInterval(() => {
+  store.counter++
+}, 0)
+
 class AppFrame extends Component {
-  @observable counter = 0
-
-  componentDidMount() {
-    setInterval(() => {
-      this.counter++
-    }, 0)
-  }
-
   ren() {
-    return <div>{this.counter}</div>
+    return <div>{store.counter}</div>
   }
 }
 
--- packages/one-do-2/src/lib/CheckBtn.js
+++ packages/one-do-2/src/lib/CheckBtn.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Btn} from '../lib/Btn'
+import {Btn} from './Btn'
 import CheckBoxBlankIcon from '@material-ui/icons/CheckCircleOutlineRounded'
 import CheckBoxCheckedIcon from '@material-ui/icons/CheckCircleRounded'
-import {ifElse_} from '../lib/little-ramda'
+import {ifElse_} from './little-ramda'
 
 const CheckBtn = ({checked, ...other}) => (
   <Btn {...other}>
