31427928ee99bc0e85966f1acacf5a7e4fbf4586
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-14 16:56:10 +0530

    one-do

--- packages/one-do/src/StoreContext.js
+++ packages/one-do/src/StoreContext.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import store from './mst-models/store'
 import {observer} from './lib/little-react'
+import {mergeAll} from './lib/ramda'
 
 const StoreContext = React.createContext(store)
 const StoreContentConsumer = StoreContext.Consumer
@@ -11,6 +12,21 @@ export const withStore = BaseComponent => {
     return (
       <StoreContentConsumer>
         {store => <BaseComponentObserver {...props} store={store} />}
+      </StoreContentConsumer>
+    )
+  }
+}
+
+export const withStoreProps = propsFn => BaseComponent => {
+  const BaseComponentObserver = observer(BaseComponent)
+  return function withStore(props) {
+    return (
+      <StoreContentConsumer>
+        {store => (
+          <BaseComponentObserver
+            {...mergeAll([{store}, props, propsFn(store, props)])}
+          />
+        )}
       </StoreContentConsumer>
     )
   }
