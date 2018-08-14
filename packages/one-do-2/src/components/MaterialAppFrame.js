e48f227de12fb4c038615647db7ef8527d66122a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-14 20:10:14 +0530

    one-do-2

--- packages/one-do-2/src/components/MaterialAppFrame.js
+++ packages/one-do-2/src/components/MaterialAppFrame.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {FocusTrap, observer} from '../lib/little-react'
+import {
+  FocusTrap,
+  Fr,
+  observer,
+  whenKey,
+  withKeyEvent,
+} from '../lib/little-react'
+
+import cn from 'classnames'
 import {compose} from '../lib/ramda'
+import MenuIcon from '@material-ui/icons/MenuRounded'
+import IconButton from '@material-ui/core/IconButton/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment/InputAdornment'
+import Button from '@material-ui/core/Button/Button'
 import withWidth, {isWidthUp} from '@material-ui/core/withWidth/withWidth'
 import withStyles from '@material-ui/core/styles/withStyles'
+import Typography from '@material-ui/core/Typography/Typography'
+import Toolbar from '@material-ui/core/Toolbar/Toolbar'
+import Drawer from '@material-ui/core/Drawer/Drawer'
 import AppBar from '@material-ui/core/AppBar/AppBar'
+import Dialog from '@material-ui/core/Dialog/Dialog'
+import DialogActions from '@material-ui/core/DialogActions/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle'
+import TextField from '@material-ui/core/TextField/TextField'
 import {afterMountAndUpdate} from '../lib/little-recompose'
+import {DrawerTaskLists} from './DrawerTaskLists'
 import {withStore} from '../StoreContext'
+import {handleUpdateItem} from '../mst-models/StoreActionsHandlers'
 import {EditTaskModal} from './EditTaskModal'
 import BottomBar from './BottomBar'
-import {xStore} from '../mst-models/RootStore'
+import {
+  handleCloseDrawer,
+  handleCloseDrawerIfTemporary,
+  handleToggleDrawer,
+  xStore,
+} from '../mst-models/RootStore'
+import {drawerWidth} from './constants'
 import SelectedListContent from './SelectedListContent'
 import GlobalEventListener from './GlobalEventListener'
-import {EditListModal} from './EditListModal'
-import {SideBar} from './SideBar'
-import {TopToolBar} from './TopToolBar'
 
 const styles = theme => ({
   root: {
@@ -42,6 +67,62 @@ const styles = theme => ({
 const contentLookup = {
   SelectedList: SelectedListContent,
   AllLists: SelectedListContent,
+}
+
+@observer
+class TopToolBar extends Component {
+  render() {
+    return (
+      <Toolbar>
+        <IconButton color={'inherit'} onClick={handleToggleDrawer}>
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          className={cn('ml2')}
+          variant="title"
+          color="inherit"
+          noWrap
+        >
+          ONE DO
+        </Typography>
+      </Toolbar>
+    )
+  }
+}
+
+@withStyles(theme => ({
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerPaperClosed: {
+    position: 'relative',
+    width: 0,
+  },
+}))
+@withStore
+class SideBar extends Component {
+  render() {
+    const {store, classes} = this.props
+    const isDrawerOpen = xStore.isDrawerOpen
+    return (
+      <Drawer
+        variant={xStore.drawerVariant}
+        classes={{
+          paper: isDrawerOpen
+            ? classes.drawerPaper
+            : classes.drawerPaperClosed,
+        }}
+        open={isDrawerOpen}
+        onClose={handleCloseDrawer}
+        onClick={handleCloseDrawerIfTemporary}
+        ModalProps={{keepMounted: true}}
+      >
+        <TopToolBar />
+        <DrawerTaskLists store={store} />
+      </Drawer>
+    )
+  }
 }
 
 @compose(
@@ -88,3 +169,56 @@ MaterialAppFrame.propTypes = {
 
 export default withStyles(styles)(MaterialAppFrame)
 
+@observer
+class EditListModal extends Component {
+  handleClose = () => {
+    this.props.store.endEditList()
+  }
+  render() {
+    const {store} = this.props
+    const {editingList: list} = store
+    return (
+      <Fr>
+        {list && (
+          <Dialog
+            // fullScreen={store.isMobileLayout}
+            open={true}
+            onClose={this.handleClose}
+            maxWidth={'xs'}
+            fullWidth
+          >
+            <DialogTitle>{'Edit List'}</DialogTitle>
+            <DialogContent>
+              <TextField
+                fullWidth
+                autoFocus={true}
+                type="text"
+                value={list.name}
+                onKeyDown={withKeyEvent(
+                  whenKey('enter')(this.handleClose),
+                )}
+                onChange={e =>
+                  handleUpdateItem({name: e.target.value}, list)(e)
+                }
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Typography variant={'headline'} color={'error'}>
+                        {list.isDirty && `*`}
+                      </Typography>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Close
+              </Button>
+            </DialogActions>
+          </Dialog>
+        )}
+      </Fr>
+    )
+  }
+}
