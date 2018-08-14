d778cf1bdfe2b300e9e2affbe074f145ac3b671b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-14 17:32:43 +0530

    one-do

--- packages/one-do/src/components/BottomBar.js
+++ packages/one-do/src/components/BottomBar.js
@@ -1,48 +1,76 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import cn from 'classnames'
 import {fWord} from '../lib/fake'
 import {
-  handleAddTask,
-  handleDeleteItem,
+  handleToggleDrawer,
   handleUpdateItem,
 } from '../mst-models/StoreActionsHandlers'
 import {FlexRow} from './UI'
 import {IconBtn} from '../lib/IconBtn'
 import {AddIcon, DeleteIcon, DoneIcon, MenuIcon} from './Icons'
 import {withStore} from '../StoreContext'
-import {handleToggleDrawer} from '../mst-models/StoreActionsHandlers'
+import {computed} from '../lib/mobx'
+import {isNil} from '../lib/ramda'
 
-function BottomBar({store: {selectedTask: task}}) {
-  return (
-    <FlexRow className={cn('pa2 flex-shrink-0', 'bt b--moon-gray')}>
-      <IconBtn
-        Icon={DeleteIcon}
-        label={'delete'}
-        disabled={Boolean(!task)}
-        onClick={handleDeleteItem(task)}
-      />
-      <div className={cn('flex-auto')} />
-      <IconBtn
-        Icon={MenuIcon}
-        label={'menu'}
-        onClick={handleToggleDrawer()}
-      />
-      <IconBtn
-        Icon={AddIcon}
-        label={'add'}
-        onClick={e => handleAddTask({name: fWord()})(e)}
-      />
-      <IconBtn
-        Icon={DoneIcon}
-        label={'done'}
-        disabled={Boolean(!task)}
-        onClick={
-          task ? handleUpdateItem({isDone: !task.isDone}, task) : null
-        }
-      />
-    </FlexRow>
-  )
+class BottomBar extends Component {
+  @computed
+  get selectedTask() {
+    return this.props.store.selectedTask
+  }
+
+  @computed
+  get store() {
+    return this.props.store
+  }
+
+  @computed
+  get isTaskSelectionEmpty() {
+    return isNil(this.selectedTask)
+  }
+
+  handleDeleteTask = () => this.store.deleteItem(this.selectedTask)
+
+  handleAddTask = () => this.store.addTask({name: fWord()})
+
+  handleDoneTask = () => {
+    return this.selectedTask
+      ? handleUpdateItem(
+          {isDone: !this.selectedTask.isDone},
+          this.selectedTask,
+        )
+      : null
+  }
+
+  render() {
+    return (
+      <FlexRow className={cn('pa2 flex-shrink-0', 'bt b--moon-gray')}>
+        <IconBtn
+          Icon={DeleteIcon}
+          label={'delete'}
+          disabled={this.isTaskSelectionEmpty}
+          onClick={this.handleDeleteTask}
+        />
+        <div className={cn('flex-auto')} />
+        <IconBtn
+          Icon={MenuIcon}
+          label={'menu'}
+          onClick={handleToggleDrawer()}
+        />
+        <IconBtn
+          Icon={AddIcon}
+          label={'add'}
+          onClick={this.handleAddTask}
+        />
+        <IconBtn
+          Icon={DoneIcon}
+          label={'done'}
+          disabled={this.isTaskSelectionEmpty}
+          onClick={this.handleDoneTask}
+        />
+      </FlexRow>
+    )
+  }
 }
 
 BottomBar.propTypes = {}
