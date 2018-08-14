d8272b12d5d9d4c557dbc616667cb56feb9d4744
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-14 16:57:33 +0530

    one-do

--- packages/one-do/src/StoreContext.js
+++ packages/one-do/src/StoreContext.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import store from './mst-models/store'
 import {observer} from './lib/little-react'
-import {mergeAll} from './lib/ramda'
+import {always, mergeAll} from './lib/ramda'
 
 const StoreContext = React.createContext(store)
 const StoreContentConsumer = StoreContext.Consumer
-
-export const withStore = BaseComponent => {
-  const BaseComponentObserver = observer(BaseComponent)
-  return function withStore(props) {
-    return (
-      <StoreContentConsumer>
-        {store => <BaseComponentObserver {...props} store={store} />}
-      </StoreContentConsumer>
-    )
-  }
-}
 
 export const withStoreProps = propsFn => BaseComponent => {
   const BaseComponentObserver = observer(BaseComponent)
@@ -31,6 +20,8 @@ export const withStoreProps = propsFn => BaseComponent => {
     )
   }
 }
+export const withStore = withStoreProps(always({}))
+
 export function StoreContextProvider({children}) {
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
