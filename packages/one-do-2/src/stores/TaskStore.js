fc8da0225a7897cdbf4bebe88e4a3655d927f7d3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 13:53:21 +0530

    one-do-2

--- packages/one-do-2/src/stores/TaskStore.js
+++ packages/one-do-2/src/stores/TaskStore.js
@@ -1,9 +1,19 @@
-import {observable} from '../lib/mobx'
+import {action, observable} from '../lib/mobx'
 import {autobind} from '../lib/autobind'
+import {nanoid} from '../lib/nanoid'
+
+class Task {
+  id = `Task_${nanoid()}`
+}
 
 @autobind
 class TaskStore {
   @observable tasks = []
+
+  @action
+  addTask() {
+    this.tasks.unshift(new Task())
+  }
 }
 
 export default TaskStore
