0a2e201df5e688e73fed4c4a7ff434da5614b2b4
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 16:56:46 +0530

    one-do-2

--- packages/one-do-2/src/components/TaskListItem.js
+++ packages/one-do-2/src/components/TaskListItem.js
@@ -2,9 +2,20 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {cn, observer} from '../lib/little-react'
 import {FlexRow} from '../lib/UI'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import {observable} from '../lib/mobx'
 
 @observer
 class TaskListItem extends Component {
+  @observable anchorEl = null
+
+  onMenuOpen = e => {
+    this.anchorEl = e.currentTarget
+  }
+  handleClose = () => {
+    this.anchorEl = null
+  }
   render() {
     const {task} = this.props
     return (
@@ -18,7 +29,19 @@ class TaskListItem extends Component {
         <div className={cn('ph1 flex-auto', 'f4 ', 'bb b--moon-gray')}>
           {task.title}
         </div>
-        <div className={cn('ph2', 'pointer')}>...</div>
+        <div className={cn('ph2', 'pointer')} onClick={this.onMenuOpen}>
+          ...
+        </div>
+        <Menu
+          id="simple-menu"
+          anchorEl={this.anchorEl}
+          open={Boolean(this.anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+          <MenuItem onClick={this.handleClose}>My account</MenuItem>
+          <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+        </Menu>
       </FlexRow>
     )
   }
