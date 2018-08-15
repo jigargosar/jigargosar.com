b32c15f086a40945dbbdb18fc411f7dd5f2421dd
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 08:45:58 +0530

    one-do-2

--- packages/one-do-2/src/store.js
+++ packages/one-do-2/src/store.js
@@ -1,9 +1,14 @@
 import {observable} from 'mobx'
 import {Disposers} from './lib/little-mst'
 import {mobxStorage} from './lib/little-mobx'
+import {toJS} from './lib/mobx'
 
 export const store = observable({})
 const disposers = Disposers(module)
 
 const rootStorage = mobxStorage(store, 'rootStore', disposers)
 rootStorage.loadAndStart()
+
+function storeAsJSON() {
+  return toJS(store)
+}
