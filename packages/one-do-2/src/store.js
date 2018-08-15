0aab031b8b2a7adee27c26a7a1e2c0c4849602df
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 10:29:30 +0530

    one-do-2

--- packages/one-do-2/src/store.js
+++ packages/one-do-2/src/store.js
@@ -1,13 +1,18 @@
 import {autobind} from './lib/little-react'
 import {observable, toJS} from './lib/mobx'
 import {Disposers} from './lib/little-mobx'
+import {pick} from './lib/ramda'
 
 @autobind
 class Store {
   @observable title = 'One Do'
   disposers = Disposers(module)
+
   get asJSON() {
     return JSON.stringify(toJS(this), null, 2)
+  }
+  get snapshotForLS() {
+    return pick(['title'], toJS(this))
   }
 }
 
