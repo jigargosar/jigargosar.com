57b5ee6868a701bc7d82a57eeb911013f7ec86a8
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 16:21:12 +0530

    one-do-2

--- packages/one-do-2/src/components/StoreJSON.js
+++ packages/one-do-2/src/components/StoreJSON.js
@@ -1,22 +1,20 @@
 import React, {Component, Fragment} from 'react'
 import cn from 'classnames'
-import {debugStore, store} from '../stores'
+import {store} from '../stores'
 import {observer} from '../lib/little-react'
 
 @observer
 class StoreJSON extends Component {
   render() {
     return (
-      debugStore.isDebugViewOpen && (
-        <Fragment>
-          <h1 className={cn('ma3')}>Store JSON</h1>
-          <div className={cn('ma3')}>
-            <pre className={cn('pa3 bg-light-gray')}>
-              <code className={cn('code')}>{store.toJSON}</code>
-            </pre>
-          </div>
-        </Fragment>
-      )
+      <Fragment>
+        <h1 className={cn('ma3')}>Store JSON</h1>
+        <div className={cn('ma3')}>
+          <pre className={cn('pa3 bg-light-gray')}>
+            <code className={cn('code')}>{store.toJSON}</code>
+          </pre>
+        </div>
+      </Fragment>
     )
   }
 }
