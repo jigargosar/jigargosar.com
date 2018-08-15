4a1cc5b564cf9183072880b7ece1d8f335b38ae9
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 22:23:45 +0530

    one-do-2

--- packages/one-do-2/src/components/TaskListItem.js
+++ packages/one-do-2/src/components/TaskListItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {cn, observer} from '../lib/little-react'
 import {FlexRow} from '../lib/UI'
 import {taskViewStore} from '../stores'
+import FocusChild from '../lib/FocusChild'
 
 @observer
 class TaskListItem extends Component {
@@ -13,19 +14,21 @@ class TaskListItem extends Component {
   render() {
     const {task} = this.props
     return (
-      <FlexRow
-        className={cn('ph1', 'link', {'bg-light-blue': task.isSelected})}
-        tabIndex={task.isSelected ? 0 : -1}
-        onFocus={this.handleOnFocus(task)}
-      >
-        <div
-          className={cn('pa2', 'code usn pointer')}
-          onClick={task.toggleDone}
+      <FocusChild shouldFocus={task.isSelected}>
+        <FlexRow
+          className={cn('ph1', 'link', {'bg-light-blue': task.isSelected})}
+          tabIndex={task.isSelected ? 0 : -1}
+          onFocus={this.handleOnFocus(task)}
         >
-          {task.isDone ? `[x]` : `[ ]`}
-        </div>
-        <div className={cn('pa2 flex-auto', 'f4 ')}>{task.title}</div>
-      </FlexRow>
+          <div
+            className={cn('pa2', 'code usn pointer')}
+            onClick={task.toggleDone}
+          >
+            {task.isDone ? `[x]` : `[ ]`}
+          </div>
+          <div className={cn('pa2 flex-auto', 'f4 ')}>{task.title}</div>
+        </FlexRow>
+      </FocusChild>
     )
   }
 }
