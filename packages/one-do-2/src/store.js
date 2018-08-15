a2954abbea2b92edf366d124cf962b38fe933edc
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 08:42:56 +0530

    one-do-2

--- packages/one-do-2/src/store.js
+++ packages/one-do-2/src/store.js
@@ -3,11 +3,6 @@ import {Disposers} from './lib/little-mst'
 import {mobxStorage} from './lib/little-mobx'
 
 export const store = observable({})
-
-function incCounter() {
-  store.counter++
-}
-
 const disposers = Disposers(module)
 
 const rootStorage = mobxStorage(store, 'rootStore', disposers)
