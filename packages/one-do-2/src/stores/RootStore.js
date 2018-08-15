d51236519e6888685a2be74b8f0a777066c797f1
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 14:37:25 +0530

    one-do-2

--- packages/one-do-2/src/stores/RootStore.js
+++ packages/one-do-2/src/stores/RootStore.js
@@ -1,6 +1,5 @@
 import {computed, observable, toJS} from '../lib/mobx'
 import {prettyJSONStringify} from '../lib/little-ramda'
-import {merge, pick} from '../lib/ramda'
 import {autobind} from '../lib/autobind'
 import TaskStore from './TaskStore'
 
@@ -9,24 +8,17 @@ class RootStore {
   @observable title = 'One Do'
   @observable taskStore = null
   constructor() {
-    this.taskStore = new TaskStore({root: this})
+    this.taskStore = new TaskStore()
   }
 
   @computed
   get toJSON() {
-    return prettyJSONStringify(this.toJSForLS)
+    return prettyJSONStringify(this.toJS)
   }
 
   @computed
   get toJS() {
     return toJS(this)
-  }
-  @computed
-  get toJSForLS() {
-    return merge(
-      {taskStore: this.taskStore.toJSForLS},
-      pick(['title'], this),
-    )
   }
 }
 
--- packages/one-do-2/src/stores/TaskStore.js
+++ packages/one-do-2/src/stores/TaskStore.js
@@ -1,9 +1,8 @@
-import {action, computed, observable} from '../lib/mobx'
+import {action, observable} from '../lib/mobx'
 import {nanoid} from '../lib/nanoid'
 import {fWord} from '../lib/fake'
 import {setter} from 'mobx-decorators'
 import {autobind} from '../lib/autobind'
-import {merge, pick} from '../lib/ramda'
 
 @autobind
 class Task {
@@ -21,20 +20,10 @@ class Task {
 @autobind
 class TaskStore {
   @observable tasks = []
-  @observable root
-
-  constructor({root}) {
-    this.root = root
-  }
 
   @action
   addNewTask() {
     this.tasks.unshift(new Task())
-  }
-
-  @computed
-  get toJSForLS() {
-    return merge({}, pick(['tasks'], this))
   }
 }
 
