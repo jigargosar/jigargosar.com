6d729e628bf343bdd2309ec749a11aca3fefa63c
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 16:14:25 +0530

    one-do-2

--- packages/one-do-2/src/components/StoreJSON.js
+++ packages/one-do-2/src/components/StoreJSON.js
@@ -1,21 +1,26 @@
 import React, {Component, Fragment} from 'react'
 import cn from 'classnames'
-import {rootStore, store} from '../stores'
+import {debugStore, rootStore, store} from '../stores'
 import {EventListener, observer} from '../lib/little-react'
 
 @observer
 class StoreJSON extends Component {
   render() {
     return (
-      <Fragment>
-        <EventListener target={document} onKeyDown={rootStore.onKeyDown} />
-        <h1 className={cn('ma3')}>Store JSON</h1>
-        <div className={cn('ma3')}>
-          <pre className={cn('pa3 bg-light-gray')}>
-            <code className={cn('code')}>{store.toJSON}</code>
-          </pre>
-        </div>
-      </Fragment>
+      debugStore.isDebugViewOpen && (
+        <Fragment>
+          <EventListener
+            target={document}
+            onKeyDown={rootStore.onKeyDown}
+          />
+          <h1 className={cn('ma3')}>Store JSON</h1>
+          <div className={cn('ma3')}>
+            <pre className={cn('pa3 bg-light-gray')}>
+              <code className={cn('code')}>{store.toJSON}</code>
+            </pre>
+          </div>
+        </Fragment>
+      )
     )
   }
 }
