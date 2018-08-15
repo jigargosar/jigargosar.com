a7798ce440cc9156d09fde825de792cc6be1a6ea
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 13:57:25 +0530

    one-do-2

--- packages/one-do-2/src/stores/index.js
+++ packages/one-do-2/src/stores/index.js
@@ -2,7 +2,18 @@ import './spy'
 import RootStore from './RootStore'
 import {observable} from '../lib/mobx'
 import TaskStore from './TaskStore'
+import {Disposers, mobxStorage} from '../lib/little-mobx'
+import {identity} from '../lib/ramda'
 
 export const tasksStore = observable(new TaskStore())
 export const rootStore = observable(new RootStore({tasksStore}))
 export const store = rootStore
+
+const rootStoreLS = mobxStorage({
+  store: rootStore,
+  key: 'rootStore',
+  disposers: Disposers(module),
+  preProcessStorageJS: identity,
+})
+
+rootStoreLS.loadAndStart()
