29757756ce24fa6c73a78a2ad9d620f9b71a9260
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 13:54:20 +0530

    one-do-2

--- packages/one-do-2/src/stores/TaskStore.js
+++ packages/one-do-2/src/stores/TaskStore.js
@@ -1,9 +1,11 @@
 import {action, observable} from '../lib/mobx'
 import {autobind} from '../lib/autobind'
 import {nanoid} from '../lib/nanoid'
+import {fWord} from '../lib/fake'
 
 class Task {
   id = `Task_${nanoid()}`
+  name = fWord()
 }
 
 @autobind
