78451c84198ea5e623612c30074e8a2466e2de1e
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 13:32:05 +0530

    one-do-2

---
+++ packages/one-do-2/src/stores/RootStore.js
@@ -0,0 +1,27 @@
+import {computed, observable, toJS} from '../lib/mobx'
+import {Disposers} from '../lib/little-mobx'
+import {prettyJSONStringify} from '../lib/little-ramda'
+import {pick} from '../lib/ramda'
+import {autobind} from '../lib/autobind'
+
+@autobind
+class RootStore {
+  @observable title = 'One Do'
+  disposers = Disposers(module)
+
+  @computed
+  get toJSON() {
+    return prettyJSONStringify(this.toJS)
+  }
+
+  @computed
+  get toJS() {
+    return toJS(this)
+  }
+  @computed
+  get toJSForLS() {
+    return pick(['title'], toJS(this))
+  }
+}
+
+export default RootStore
--- packages/one-do-2/src/stores/index.js
+++ packages/one-do-2/src/stores/index.js
@@ -1,1 +1,5 @@
-export {default as store} from '../store'
+import './spy'
+import RootStore from './RootStore'
+import {observable} from '../lib/mobx'
+
+export const store = observable(new RootStore())
