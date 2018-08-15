6fd2c1abb9bbb50b6eaebb952134cc037461e252
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 22:06:43 +0530

    one-do-2

--- packages/one-do-2/src/components/TaskListItem.js
+++ packages/one-do-2/src/components/TaskListItem.js
@@ -14,17 +14,17 @@ class TaskListItem extends Component {
     const {task} = this.props
     return (
       <FlexRow
-        className={cn('pv2', 'link', {'bg-light-blue': task.isSelected})}
+        className={cn('ph1', 'link', {'bg-light-blue': task.isSelected})}
         tabIndex={task.isSelected ? 0 : -1}
         onFocus={this.handleOnFocus(task)}
       >
         <div
-          className={cn('pr1 mr1', 'code usn pointer')}
+          className={cn('pa2', 'code usn pointer')}
           onClick={task.toggleDone}
         >
           {task.isDone ? `[x]` : `[ ]`}
         </div>
-        <div className={cn('ph1 flex-auto', 'f4 ')}>{task.title}</div>
+        <div className={cn('pa2 flex-auto', 'f4 ')}>{task.title}</div>
       </FlexRow>
     )
   }
