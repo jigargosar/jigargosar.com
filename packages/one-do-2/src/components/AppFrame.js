fd0aa4c86943b6146b72d74bde1d0f3fa152269b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 08:51:40 +0530

    one-do-2

--- packages/one-do-2/src/components/AppFrame.js
+++ packages/one-do-2/src/components/AppFrame.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import {Component} from '../lib/little-mobx-react'
 import {storeAsJSON} from '../store'
+import {cn, Fr} from '../lib/little-react'
 
 class AppFrame extends Component {
   ren() {
-    return <div>{storeAsJSON()}</div>
+    return (
+      <Fr>
+        <h1 className={cn('ma3')}>Store JSON</h1>
+        <div className={cn('ma3')}>
+          <pre className={cn('pa3 bg-light-gray')}>
+            <code className={cn('code')}>{storeAsJSON()}</code>
+          </pre>
+        </div>
+      </Fr>
+    )
   }
 }
 
