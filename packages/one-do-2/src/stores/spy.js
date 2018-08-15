fa2f1aadb48177918ed15d7f11a08c69b3222ce8
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 22:50:46 +0530

    one-do-2

--- packages/one-do-2/src/stores/spy.js
+++ packages/one-do-2/src/stores/spy.js
@@ -3,5 +3,5 @@ import {Disposers} from '../lib/little-mobx'
 const disposers = Disposers(module)
 
 disposers.spy(change => {
-  console.debug(`change`, change)
+  // console.debug(`change`, change)
 })
