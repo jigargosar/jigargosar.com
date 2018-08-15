d8a148eba83df5b997284cd139c1f64c506d11be
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 09:10:35 +0530

    one-do-2

--- packages/one-do-2/src/components/AppFrame.js
+++ packages/one-do-2/src/components/AppFrame.js
@@ -1,22 +1,6 @@
 import React from 'react'
 import {Component} from '../lib/little-mobx-react'
-import {store} from '../store'
-import {cn, Fr} from '../lib/little-react'
-
-class StoreJSON extends Component {
-  ren() {
-    return (
-      <Fr>
-        <h1 className={cn('ma3')}>Store JSON</h1>
-        <div className={cn('ma3')}>
-          <pre className={cn('pa3 bg-light-gray')}>
-            <code className={cn('code')}>{store.asJSON}</code>
-          </pre>
-        </div>
-      </Fr>
-    )
-  }
-}
+import StoreJSON from './StoreJSON'
 
 class AppFrame extends Component {
   ren() {
---
+++ packages/one-do-2/src/components/StoreJSON.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Component} from '../lib/little-mobx-react'
+import {Fr} from '../lib/little-react'
+import cn from 'classnames'
+import {store} from '../store'
+
+class StoreJSON extends Component {
+  ren() {
+    return (
+      <Fr>
+        <h1 className={cn('ma3')}>Store JSON</h1>
+        <div className={cn('ma3')}>
+          <pre className={cn('pa3 bg-light-gray')}>
+            <code className={cn('code')}>{store.asJSON}</code>
+          </pre>
+        </div>
+      </Fr>
+    )
+  }
+}
+
+export default StoreJSON
