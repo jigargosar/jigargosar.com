7e4e9037b53d5865b4baacf9867ff77ecb5a786f
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 15:27:07 +0530

    one-do-2

--- packages/one-do-2/src/components/TaskListItem.js
+++ packages/one-do-2/src/components/TaskListItem.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {observer} from '../lib/little-react'
+import {cn, observer} from '../lib/little-react'
 
 @observer
 class TaskListItem extends Component {
   render() {
     const {task} = this.props
-    return <div>{task.title}</div>
+    return <div className={cn('')}>{task.title}</div>
   }
 }
 
