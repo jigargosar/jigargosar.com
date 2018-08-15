2c91e85f27db80fe06718a6f579fff615703d7db
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-15 21:44:55 +0530

    one-do-2

--- packages/one-do-2/src/components/AppFrame.js
+++ packages/one-do-2/src/components/AppFrame.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {Btn} from '../lib/Btn'
 import {rootStore, taskStore} from '../stores'
 import {FlexRow} from '../lib/UI'
-import {cn, EventListener, observer} from '../lib/little-react'
+import {cn, EventListener, FocusTrap, observer} from '../lib/little-react'
 import TaskList from './TaskList'
 import DebugDialog from './DebugDialog'
 
@@ -12,12 +12,17 @@ class AppFrame extends Component {
   render() {
     return (
       <Fragment>
-        <DebugDialog />
-        <EventListener target={document} onKeyDown={rootStore.onKeyDown} />
-        <FlexRow className={cn('ma3')}>
-          <Btn onClick={taskStore.addNewTask}>Add New Task</Btn>
-        </FlexRow>
-        <TaskList />
+        <FocusTrap>
+          <DebugDialog />
+          <EventListener
+            target={document}
+            onKeyDown={rootStore.onKeyDown}
+          />
+          <FlexRow className={cn('ma3')}>
+            <Btn onClick={taskStore.addNewTask}>Add New Task</Btn>
+          </FlexRow>
+          <TaskList />
+        </FocusTrap>
       </Fragment>
     )
   }
