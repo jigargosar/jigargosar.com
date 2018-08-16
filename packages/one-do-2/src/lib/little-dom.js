731fe40697d73a63af4f4f69249463d6b5e9bbce
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-16 14:46:49 +0530

    one-do-2

--- packages/one-do-2/src/lib/little-dom.js
+++ packages/one-do-2/src/lib/little-dom.js
@@ -2,6 +2,14 @@ export function targetRole(e) {
   return e.target.getAttribute('role')
 }
 
+export function targetTagName(e) {
+  return e.target.tagName
+}
+
 export function isTargetRoleButton(e) {
   return 'button' === targetRole(e)
+}
+
+export function isTargetTagButton(e) {
+  return 'button' === targetTagName(e)
 }
