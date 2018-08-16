982076c7a8f8578ea898c4a468a19ffd94da80d7
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-16 10:33:26 +0530

    one-do-2

--- packages/one-do-2/src/components/AppFrame.js
+++ packages/one-do-2/src/components/AppFrame.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {rootStore, taskViewStore} from '../stores'
 import {EventListener, observer} from '../lib/little-react'
 import DebugDialog from './DebugDialog'
+import {FocusTrap} from '../lib/focus-trap-react'
 import TaskListScreen from './TaskListScreen'
 
 @observer
@@ -13,26 +14,29 @@ class AppFrame extends Component {
   render() {
     return (
       <Fragment>
-        {/*<FocusTrap*/}
-        {/*paused={rootStore.isFocusTrapPaused}*/}
-        {/*focusTrapOptions={{*/}
-        {/*returnFocusOnDeactivate: false,*/}
-        {/*escapeDeactivates: false,*/}
-        {/*fallbackFocus: document,*/}
-        {/*// onActivate() {*/}
-        {/*//   console.log('onActivate')*/}
-        {/*// },*/}
-        {/*// onDeactivate() {*/}
-        {/*//   console.log('onDeactivate')*/}
-        {/*// },*/}
-        {/*}}*/}
-        {/*// active={!rootStore.isFocusTrapPaused}*/}
-        {/*active={true}*/}
-        {/*>*/}
-        <DebugDialog />
-        <EventListener target={document} onKeyDown={rootStore.onKeyDown} />
-        <TaskListScreen />
-        {/*</FocusTrap>*/}
+        <FocusTrap
+          paused={rootStore.isFocusTrapPaused}
+          focusTrapOptions={{
+            returnFocusOnDeactivate: false,
+            escapeDeactivates: false,
+            fallbackFocus: document,
+            // onActivate() {
+            //   console.log('onActivate')
+            // },
+            // onDeactivate() {
+            //   console.log('onDeactivate')
+            // },
+          }}
+          // active={!rootStore.isFocusTrapPaused}
+          active={true}
+        >
+          <DebugDialog />
+          <EventListener
+            target={document}
+            onKeyDown={rootStore.onKeyDown}
+          />
+          <TaskListScreen />
+        </FocusTrap>
       </Fragment>
     )
   }
