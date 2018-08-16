ee20b9c1a0a9a9742a315a271045cb43660cde6b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-16 18:02:46 +0530

    one-do-2

--- packages/one-do-2/src/components/AddTaskBar.js
+++ packages/one-do-2/src/components/AddTaskBar.js
@@ -2,16 +2,21 @@ import React, {Component, Fragment} from 'react'
 import {addTaskView} from '../stores'
 
 import {TextField} from '@material-ui/core'
-import {observer} from '../lib/little-react'
+import {cn, observer} from '../lib/little-react'
 
 class AddTaskBar extends Component {
   render() {
     return (
       <Fragment>
-        <TextField
-          value={addTaskView.title}
-          onChange={addTaskView.onTitleChange}
-        />
+        <div className={cn('flex pa1')}>
+          <TextField
+            className={cn('br-pill ba ph2')}
+            InputProps={{disableUnderline: true}}
+            fullWidth
+            value={addTaskView.title}
+            onChange={addTaskView.onTitleChange}
+          />
+        </div>
       </Fragment>
     )
   }
