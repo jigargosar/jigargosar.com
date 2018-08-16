ef35c23c96ac22c13c765b1a663f6d7803de4d49
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-16 11:38:18 +0530

    one-do-2

--- packages/one-do-2/src/stores/DebugStore.js
+++ packages/one-do-2/src/stores/DebugStore.js
@@ -4,11 +4,11 @@ import {compose, defaultTo, mergeWith} from '../lib/ramda'
 import {toggle} from 'mobx-decorators'
 import {observe, setter} from '../lib/mobx-decorators'
 
-const logNewValue = observe(change => {
+const logChange = change => {
   const {type, object, oldValue, newValue} = change
   console.log(`[${type}] ${object.name} ${oldValue} -> ${newValue}`)
   console.debug(change)
-})
+}
 
 @autobind
 class DebugStore {
@@ -16,7 +16,7 @@ class DebugStore {
   @observable
   isDebugViewOpen = false
 
-  @logNewValue
+  @observe(logChange, true)
   @setter('onEnter', true)
   @setter('onExited', false)
   @observable
