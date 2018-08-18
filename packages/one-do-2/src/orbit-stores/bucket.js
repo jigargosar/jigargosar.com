f6e7f3afa61e982aecf380a6a3c1ea78c6bc3e2f
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 15:40:04 +0530

    one-do-2

---
+++ packages/one-do-2/src/orbit-stores/bucket.js
@@ -0,0 +1,7 @@
+import LocalStorageBucket from '@orbit/local-storage-bucket'
+import IndexedDBBucket, {supportsIndexedDB} from '@orbit/indexeddb-bucket'
+const BucketClass = supportsIndexedDB()
+  ? IndexedDBBucket
+  : LocalStorageBucket
+
+export const bucket = new BucketClass({namespace: 'od2'})
