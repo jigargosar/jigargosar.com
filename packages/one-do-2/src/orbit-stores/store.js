5b69a4e70bd5a001981dcd5754ebcb68910eaf0c
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 14:46:54 +0530

    one-do-2

--- packages/one-do-2/src/orbit-stores/store.js
+++ packages/one-do-2/src/orbit-stores/store.js
@@ -2,3 +2,17 @@ import Store from '@orbit/store'
 import {schema} from './schema'
 
 export const store = new Store({schema})
+
+async function testStore() {
+  await store.update(t => [
+    t.aRecord({
+      type: 'task',
+      id: 'task_1',
+      attributes: {
+        title: 'first task',
+      },
+    }),
+  ])
+}
+
+testStore().catch(console.log)
