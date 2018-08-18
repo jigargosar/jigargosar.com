51a304793d5d86dcfd7ff42865896a85c6fb35b2
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 19:16:54 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -4,10 +4,38 @@ import {observer} from '../lib/little-react'
 import {disposable} from '../lib/hoc'
 import {fetchStore} from '../orbit-stores/store'
 import {prettyJSONStringify} from '../lib/little-ramda'
-import {map} from '../lib/ramda'
+import {
+  apply,
+  chain,
+  fromPairs,
+  map,
+  partial,
+  toPairs,
+  type,
+} from '../lib/ramda'
 
+const flattenObj = obj => {
+  const go = obj_ =>
+    chain(([k, v]) => {
+      if (type(v) === 'Object' || type(v) === 'Array') {
+        return map(([k_, v_]) => [`${k}.${k_}`, v_], go(v))
+      } else {
+        return [[k, v]]
+      }
+    }, toPairs(obj_))
+
+  return fromPairs(go(obj))
+}
+
+const flattenRecord = ({attributes, ...rest}) => ({
+  ...rest,
+  ...attributes,
+})
 function logRecords(records) {
   console.log(`records`, ...records)
+  const flatRecords = map(flattenRecord)(records)
+  apply(partial(console.log, ['records']), flatRecords)
+  console.table(flatRecords)
   console.log(prettyJSONStringify(records))
 }
 
