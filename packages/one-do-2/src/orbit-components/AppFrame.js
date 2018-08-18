d780e0a59d8a9ec7f9cdf198a87723a363928575
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 18:44:23 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -4,29 +4,34 @@ import {observer} from '../lib/little-react'
 import {disposable} from '../lib/hoc'
 import {createStore} from '../orbit-stores/store'
 import {isNil} from '../lib/ramda'
+import {action, observable} from '../lib/mobx'
 
 @disposable
 @observer
 class AppFrame extends Component {
+  @observable store = null
   state = {
-    store: null,
+    // store: null,
+  }
+
+  @action
+  setStore(store) {
+    this.store = store
   }
 
   componentDidMount() {
     createStore().then(store => {
-      this.setState({store})
+      // this.setState({store})
+      this.setStore(store)
     })
-  }
-
-  get isLoadingStore() {
-    return isNil(this.state.store)
   }
 
   render() {
     return (
       <Fragment>
         <h1>Orbit Tasks</h1>
-        <div>{`isLoadingStore=${this.isLoadingStore}`}</div>
+        <div>{`isNil(this.state.store)=${isNil(this.state.store)}`}</div>
+        <div>{`isNil(this.store)=${isNil(this.store)}`}</div>
       </Fragment>
     )
   }
