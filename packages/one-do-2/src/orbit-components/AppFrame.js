fedfb6dc06d48784af997f3ab2f8c8cb483e7405
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 22:19:41 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -1,6 +1,6 @@
 import '../stores/init-mobx'
 import React, {Component} from 'react'
-import {cn, observer} from '../lib/little-react'
+import {cn, observer, renderKeyedById} from '../lib/little-react'
 import {disposable} from '../lib/hoc'
 import {createStore} from '../orbit-stores/store'
 import {findRecords, tapLogRecords} from '../orbit-stores/little-orbit'
@@ -35,3 +35,17 @@ class AppFrame extends Component {
 }
 
 export default AppFrame
+
+@observer
+class Tasks extends Component {
+  render() {
+    return renderKeyedById(Task, 'task', this.props.tasks)
+  }
+}
+@observer
+class Task extends Component {
+  render() {
+    const {task} = this.props
+    return <div>{task.attributes.title}</div>
+  }
+}
