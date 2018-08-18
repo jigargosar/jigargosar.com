26daed91842da79af03b087e3c45e168fa609680
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 18:29:35 +0530

    one-do-2

--- packages/one-do-2/src/orbit-stores/backup.js
+++
@@ -1,8 +0,0 @@
-import LocalStorageSource from '@orbit/local-storage'
-import {schema} from './schema'
-
-export const backup = new LocalStorageSource({
-  schema,
-  name: 'backup',
-  namespace: 'od2',
-})
--- packages/one-do-2/src/orbit-stores/little-orbit.js
+++
@@ -1,5 +0,0 @@
-import {curry} from '../lib/ramda'
-
-export const addRecord = curry(function addRecord(record, t) {
-  return t.addRecord(record)
-})
