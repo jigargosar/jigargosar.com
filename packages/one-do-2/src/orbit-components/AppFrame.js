ab78c1ff0285522594d25a54f3bdf396c705c4d5
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 19:06:39 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -2,13 +2,12 @@ import '../stores/init-mobx'
 import React, {Component, Fragment} from 'react'
 import {observer} from '../lib/little-react'
 import {disposable} from '../lib/hoc'
-import {createStore} from '../orbit-stores/store'
-import {isNil} from '../lib/ramda'
-import {action, observable} from '../lib/mobx'
+import {fetchStore} from '../orbit-stores/store'
 import {prettyJSONStringify} from '../lib/little-ramda'
+import {map} from '../lib/ramda'
 
 function logRecords(records) {
-  console.log(`records`, records)
+  console.log(`records`, ...records)
   console.log(prettyJSONStringify(records))
 }
 
@@ -21,32 +20,17 @@ async function fetchTasks(store) {
 @disposable
 @observer
 class AppFrame extends Component {
-  @observable store = null
-  state = {
-    // store: null,
-  }
-
-  @action
-  setStore(store) {
-    this.store = store
-  }
+  fetchStoreResult = fetchStore()
 
   componentDidMount() {
-    createStore()
-      .then(store => {
-        // this.setState({store})
-        this.setStore(store)
-      })
-      .then(() => fetchTasks(this.store))
-      .catch(console.error)
+    this.fetchStoreResult.then(fetchTasks).catch(console.error)
   }
 
   render() {
     return (
       <Fragment>
         <h1>Orbit Tasks</h1>
-        <div>{`isNil(this.state.store)=${isNil(this.state.store)}`}</div>
-        <div>{`isNil(this.store)=${isNil(this.store)}`}</div>
+        <div>{`this.storeFP.status=${this.fetchStoreResult.status}`}</div>
       </Fragment>
     )
   }
