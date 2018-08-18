eba04f3cdda0e5debc8d804e22ca0ea7574b75e9
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 17:11:02 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -1,11 +1,26 @@
 import React, {Component, Fragment} from 'react'
 import {observer} from '../lib/little-react'
+import {fromPromise} from '../lib/mobx-utils'
+import {store} from '../orbit-stores/store'
 
-@observer
+const tasksRes = fromPromise(store.query(q => q.findRecords('tasks')))
+
 class AppFrame extends Component {
   render() {
-    return <Fragment>Orbit Tasks</Fragment>
+    return (
+      <Fragment>
+        <h1>Orbit Tasks</h1>
+        {tasksRes.case({
+          pending: () => 'pending',
+          fulfilled: () => 'fulfilled',
+          rejected: e => {
+            console.log(`e`, e)
+            return 'rejected'
+          },
+        })}
+      </Fragment>
+    )
   }
 }
 
-export default AppFrame
+export default observer(AppFrame)
