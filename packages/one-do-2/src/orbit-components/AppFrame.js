063d4be0c77ba1b8371fe803bf129fba4cb68f60
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-18 18:53:47 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppFrame.js
+++ packages/one-do-2/src/orbit-components/AppFrame.js
@@ -6,6 +6,12 @@ import {createStore} from '../orbit-stores/store'
 import {isNil} from '../lib/ramda'
 import {action, observable} from '../lib/mobx'
 
+async function fetchTasks(store) {
+  const tasks = await store.query(q => q.findRecords('task'))
+  console.log(`tasks`, tasks)
+  return tasks
+}
+
 @disposable
 @observer
 class AppFrame extends Component {
@@ -20,10 +26,13 @@ class AppFrame extends Component {
   }
 
   componentDidMount() {
-    createStore().then(store => {
-      // this.setState({store})
-      this.setStore(store)
-    })
+    createStore()
+      .then(store => {
+        // this.setState({store})
+        this.setStore(store)
+      })
+      .then(() => fetchTasks(this.store))
+      .catch(console.error)
   }
 
   render() {
--- packages/one-do-2/src/orbit-stores/store.js
+++ packages/one-do-2/src/orbit-stores/store.js
@@ -2,10 +2,15 @@ import {Store} from './orbit'
 import {schema} from './schema'
 import {TaskRecord} from './TaskRecord'
 
+async function addNewTask(store) {
+  await store.update(t => t.addRecord(TaskRecord()))
+}
+
 export async function createStore() {
   console.log('creating store')
   const store = new Store({schema})
-  await store.update(t => t.addRecord(TaskRecord()))
+  await addNewTask(store)
+  await addNewTask(store)
   console.log('store created')
   return store
 }
