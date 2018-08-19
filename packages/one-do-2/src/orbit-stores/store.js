f2d033ffa291a9f21678bce1ccc2add415f04ee0
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-19 16:01:53 +0530

    one-do-2

--- packages/one-do-2/src/orbit-stores/store.js
+++ packages/one-do-2/src/orbit-stores/store.js
@@ -22,12 +22,12 @@ async function createStore() {
     listeners: event => store.listeners(event),
     on: (event, callback, binding) => {
       console.log('[store] .on', event, callback.name || callback, binding)
+      store.on(event, callback, binding)
+
       console.log(
         `[store] .listeners(${event}).length`,
         store.listeners(event).length,
       )
-
-      store.on(event, callback, binding)
       disposers.addDisposer(onAutoDisposer)
       return () => off(event, callback, binding)
 
