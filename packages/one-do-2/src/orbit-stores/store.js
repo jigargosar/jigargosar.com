11c54f27a86fa2ba4b64378597dd0e49e8eeb3c3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-19 16:05:25 +0530

    one-do-2

--- packages/one-do-2/src/orbit-stores/store.js
+++ packages/one-do-2/src/orbit-stores/store.js
@@ -3,7 +3,6 @@ import {schema} from './schema'
 import {TaskRecord} from './TaskRecord'
 import {findRecords} from './little-orbit'
 import {Disposers} from '../lib/little-mobx'
-import {hotDispose} from '../lib/hot'
 
 export function addNewTask(store) {
   return store.update(t => t.addRecord(TaskRecord()))
