04efacf2d6f43cb5477868fc047ff6ec3c630fac
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-20 16:11:36 +0530

    one-do-2

--- packages/one-do-2/src/lib/p-fun.js
+++ packages/one-do-2/src/lib/p-fun.js
@@ -1,2 +1,5 @@
+export {default as delay} from 'delay'
+export {default as PQueue} from 'p-queue'
+
 export {default as pSeries} from 'p-series'
 export {default as pEachSeries} from 'p-each-series'
