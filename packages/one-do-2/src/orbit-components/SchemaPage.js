5952aa0fd7c22f1ccdaf3c2f08525ef4bdd159c9
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-21 00:35:01 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/SchemaPage.js
+++ packages/one-do-2/src/orbit-components/SchemaPage.js
@@ -52,7 +52,7 @@ class Model extends Component {
     const modelDesc = schema.getModel(type)
     const {attributes} = modelDesc
     return (
-      <div className={cn('pb2')}>
+      <div className={cn('pb3')}>
         <div className={cn('f4 b')}>{`${type}`}</div>
         <Table
           padding={'dense'}
