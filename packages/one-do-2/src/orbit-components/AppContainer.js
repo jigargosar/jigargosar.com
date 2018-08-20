8b64ff746db0c19e98c629c0d1f2d8fd0e555b41
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-20 20:45:38 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/AppContainer.js
+++ packages/one-do-2/src/orbit-components/AppContainer.js
@@ -34,6 +34,7 @@ function getSimulationTasks({speed = 1000}) {
     async () => await updateIsDone(tasks3, true),
     async () => await delay(speed),
     async () => await updateIsDone(tasks1, false),
+    async () => await delay(speed * 2),
   ]
 
   //aaa
