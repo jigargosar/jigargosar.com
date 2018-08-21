3e22236d39ad002add023677ab08037f54d32d2d
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-21 14:45:34 +0530

    one-do-2

--- packages/one-do-2/src/orbit-store/StoreSchema.js
+++ packages/one-do-2/src/orbit-store/StoreSchema.js
@@ -1,20 +1,27 @@
 import {keys, mapObjIndexed, values} from '../lib/ramda'
 
-function SchemaModel(schema, model, type) {
-  return {
-    type,
-    attributes: model.attributes,
-  }
-}
-
 export function StoreSchema(store) {
   const schema = store.schema
   const modelLookup = mapObjIndexed((model, type) =>
-    SchemaModel(schema, model, type),
+    SchemaModel(model, type),
   )(schema.models)
   return {
     models: values(modelLookup),
     getModel: type => modelLookup[type],
     modelTypes: keys(modelLookup),
+  }
+
+  function SchemaModel(model, type) {
+    function ModelAttribute(attribute, name) {
+      return {name, attribute}
+    }
+
+    const attributeLookup = mapObjIndexed((attribute, name) =>
+      ModelAttribute(attribute, name),
+    )(model.attributes)
+    return {
+      type,
+      attributes: attributeLookup,
+    }
   }
 }
