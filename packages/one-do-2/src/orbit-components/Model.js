6ef8120faeeefc966f3856bbbe3adfd993e79247
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-22 16:12:49 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/Model.js
+++ packages/one-do-2/src/orbit-components/Model.js
@@ -1,6 +1,11 @@
 import {observer} from 'mobx-react'
 import React, {Component, Fragment} from 'react'
-import {Button, TableCell, TableSortLabel, Toolbar} from '@material-ui/core'
+import {
+  Button,
+  TableCell,
+  TableSortLabel,
+  Toolbar,
+} from '@material-ui/core'
 import {action, computed, observable} from '../lib/mobx'
 import {liveQuery, updateStore} from '../orbit-store/Store'
 import {
@@ -95,7 +100,7 @@ export class Model extends Component {
           rows={this.sortedRows}
           columns={columnsFromConfigs(this.columnConfigs)}
         />
-      </Card>
+      </Fragment>
     )
   }
 
