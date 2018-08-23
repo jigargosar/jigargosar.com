5e21ff478e8dca1cc7234d7ffc0c817294157d93
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-23 10:13:24 +0530

    one-do-2

--- packages/one-do-2/src/orbit-components/Model.js
+++ packages/one-do-2/src/orbit-components/Model.js
@@ -30,6 +30,7 @@ import cn from 'classnames'
 import {AddIcon} from '../lib/Icons'
 import DataGrid from './DataGrid'
 import {withStateHandlers} from '../lib/recompose'
+import {overPath} from '../lib/little-ramda'
 
 function attributesToColumnConfigs(attribute) {
   const name = attribute.name
@@ -107,9 +108,10 @@ export class Model extends Component {
       },
     },
     {
-      toggleSortDirection: ({sort: {direction}}) => () => ({
-        direction: direction === 'asc' ? 'desc' : 'asc',
-      }),
+      toggleSortDirection: ({sort}) => () =>
+        overPath(['sort', 'direction'])(
+          direction => (direction === 'asc' ? 'desc' : 'asc'),
+        )(sort),
       setSortState: () => objOf('sort'),
     },
   ),
