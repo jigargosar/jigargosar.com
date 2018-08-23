64daee8c4b4608a3089dc8e76aa6e321e6de7849
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-23 19:47:03 +0530

    one-do-2

--- packages/one-do-2/src/lib/fake.js
+++ packages/one-do-2/src/lib/fake.js
@@ -3,5 +3,7 @@ import f from 'faker'
 export const randomWord = () => f.random.word()
 export const randomBool = () => f.random.boolean()
 export const randomNumber = f.random.number
+let num = 10000
+export const randomNumberRange = num => randomNumber({min: -num, max: num})
 export const randomTS = () =>
-  Date.now() + randomNumber({min: -10000, max: 10000})
+  Date.now() + randomNumberRange(7 * 24 * 60 * 60 * 1000)
